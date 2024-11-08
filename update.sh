f4abc84a2c40011562b2b29e007511cf412a4858
Zozano <private@private.com>
2024-11-08 14:35:47 +1100

    2024-11-08_14:35:47

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #~/.nix-profile/bin/bash
 
 git add * .config/
-git commit -m "$(date '+$F_$H:$M:%S')"
+git commit -m "$(date '+%F_%H:%M:%S')"
 git push github main
 home-manager switch -b backup --flake ~/.dotfiles/
 
