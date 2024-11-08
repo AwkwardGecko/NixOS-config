9178dae4057be4e1f6f58521f58fffad18e3e6f5
Zozano <private@private.com>
2024-11-08 20:04:09 +1100

    2024-11-08_20:04:09

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -34,13 +34,14 @@
 		vim.g.maplocalleader = ' '
 		vim.o.clipboard = 'unnamedplus'
 		vim.o.number = true
+		-- vim.o.relativenumber = true
 		vim.o.signcolumn = 'yes'
 		vim.o.tabstop = 4
 		vim.o.shiftwidth = 4
 		vim.o.updatetime = 300
 		vim.o.mouse = 'a'
 		vim.o.termguicolors = true
-		vim.cmd('colorscheme gruvbox-material')
+		vim.cmd('colorscheme gruvbox')
 		vim.g.gruvbox_material_background = 'hard'
 	'';
   };
