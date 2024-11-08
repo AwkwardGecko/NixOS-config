090f8c7ae379feee49d040a90f05ee3ecac8a7f8
Zozano <private@private.com>
2024-11-09 10:55:28 +1100

    2024-11-09_10:55:28

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -159,21 +159,7 @@
 			
 			/*base background color*/
 			@define-color red #161320;
-			/@define-color bg_main_tooltip rgba(0, 0, 0, 0.7);
 
-
-			/*base background color of selections */
-			/@define-color bg_hover rgba(200, 200, 200, 0.3);
-			/*base background color of active elements */
-			@define-color bg_active rgba(100, 100, 100, 0.5);
-
-			/*base border color*/
-			@define-color border_main rgba(255, 255, 255, 0.2);
-
-			/*text color for entries, views and content in general */
-			@define-color content_main white;
-			/*text color for entries that are unselected */
-			@define-color content_inactive rgba(255, 255, 255, 0.25);
 
 
 			* {	
