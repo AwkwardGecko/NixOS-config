0eaaa97492280ce0aeafc5463bed49c23c2db995
Zozano <private@private.com>
2024-11-08 23:08:52 +1100

    2024-11-08_23:08:52

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -109,9 +109,9 @@
 				on-click = "pavucontrol";
 			};
 			
-			hyprland/mode = { 
-				format = ''<span style="italic">{}</span>'';
-			};
+			#hyprland/mode = { 
+			#	format = ''<span style="italic">{}</span>'';
+			#};
 
 			temperature = {
 				critical-threshold = 80;
