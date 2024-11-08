f95b1e251edfd0b6f34737c12de5364f112faf18
Zozano <private@private.com>
2024-11-09 01:14:33 +1100

    2024-11-09_01:14:33

--- home-manager/modules/waybar/mechabar.nix
+++ home-manager/modules/waybar/mechabar.nix
@@ -257,11 +257,15 @@
 					"mode" = "month";
 					"mode-mon-col" = 6;
 					"on-click-right" = "mode";
-					"format" = { "today" = "<span color='#f38ba8'><b>{}</b></span>"; };
+					"format" = { 
+						"today" = "<span color='#f38ba8'><b>{}</b></span>";
+					};
 
 				};
 				
-				"actions" = { "on-click-right": "mode" };
+				"actions" = { 
+					"on-click-right" = "mode";
+				};
 				"min-length" = 8;
 				"max-length" = 8;
 			};
