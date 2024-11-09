b1ec130fd7cbcd117bd293ade547303d4dc8a4ed
Zozano <private@private.com>
2024-11-09 11:29:31 +1100

    2024-11-09_11:29:31

---
+++ learning/tmux.nix
@@ -0,0 +1,23 @@
+
+
+
+tmux new -s <nameOfSession>
+
+tmux a #connect to last session
+
+tmux a -t <nameOfSession> #connect to specific session
+
+tmux ls # list sessions
+
+# Prefix Key
+CTRL + B (C-b)
+#detach from session
+D
+# split window horizontally
+SHIFT + 5
+# split window vertically
+SHIFT + '
+# switch tab
+Q + number
+# move pane
+hold CTRL + B, Arrows
