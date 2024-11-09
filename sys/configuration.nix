13594c1fcc55dacb7507cd2de0dc0fa728730819
Zozano <private@private.com>
2024-11-09 15:13:53 +1100

    2024-11-09_15:13:53

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -21,7 +21,28 @@
   nixpkgs.config.allowUnfree = true;
   hardware.enableAllFirmware = true;
 
-  security.polkit.enable = true;
+  security.polkit = {
+	enable = true;
+	extraConfig = ''
+		
+		polkit.addRule(function(action, subject) {
+			
+			if (
+				
+				subject.isInGroup("users")
+				&& (
+					action.id == "org.freedesktop.login1.reboot" ||
+					action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+					action.id == "org.freedesktop.login1.power-off" ||
+					action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+				)
+			)
+			
+			{
+				return polkit.Result.YES;
+			}
+		});
+	'';
 
   imports = [
     ./hardware-configuration.nix
