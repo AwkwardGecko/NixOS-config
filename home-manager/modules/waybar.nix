a1ba50395612178c4612005eb891336300c2be71
Zozano <private@private.com>
2024-11-13 08:23:47 +1100

    2024-11-13_08:23:47

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -92,7 +92,7 @@
 			"custom/power" = {
 				"format" = "⏻ ";
 				"tooltip" = true;
-				"on-click" = "wlogout";
+				"on-click" = "shutdown";
 			#	"menu" = "on-click";
 			#	"menu-file" = "~/.dotfiles/home-manager/modules/waybar/power_menu.xml"; # Menu file in resources folder
 			};
