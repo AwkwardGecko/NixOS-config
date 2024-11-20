07489f152009207ecab1b64995e0a6f8a8ec7655
Zozano <private@private.com>
2024-11-20 23:09:48 +1100

    2024-11-20_23:09:48

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -10,6 +10,7 @@
 		shellInit = "cd .dotfiles/";
 		shellInitLast = "fastfetch";
 		plugins = [
+			{ name = "gruvbox"; src = pkgs.fishPlugins.gruvbox.src;}
 			{ name = "grc"; src = pkgs.fishPlugins.grc.src; }
 		];
 	};
--- update-system.sh
+++ update-system.sh
@@ -5,4 +5,4 @@ git add *
 git commit -m "$(date '+%F_%H:%M:%S')"
 git push github main
 home-manager switch -b backup --flake ~/.dotfiles/
-#sudo nixos-rebuild switch --upgrade --flake ~/.dotfiles/
+sudo nixos-rebuild switch --upgrade --flake ~/.dotfiles/
