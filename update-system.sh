70d874a588be7de01c0da7836f2d2c71928e816e
Zozano <private@private.com>
2024-11-20 23:11:16 +1100

    2024-11-20_23:11:16

--- update-system.sh
+++
@@ -1,8 +0,0 @@
-#~/.nix-profile/bin/bash
-
-cd ~/.dotfiles
-git add *
-git commit -m "$(date '+%F_%H:%M:%S')"
-git push github main
-home-manager switch -b backup --flake ~/.dotfiles/
-sudo nixos-rebuild switch --upgrade --flake ~/.dotfiles/
