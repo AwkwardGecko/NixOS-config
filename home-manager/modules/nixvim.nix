df2719fdd24831bbe3dd0c33a10836a45ec86d19
Zozano <private@private.com>
2024-11-28 13:59:24 +1100

    2024-11-28_13:59:24

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -2,13 +2,9 @@
   config,
   pkgs,
   lib,
-  inputs,
 ...
 }:
-{
-  inputs.nixvim = {
-        url = "github:nix-community/nixvim";
-        # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
-        inputs.nixpkgs.follows = "nixpkgs";
-  };
+  
+  programs.nixvim.enable = true
+
 }
