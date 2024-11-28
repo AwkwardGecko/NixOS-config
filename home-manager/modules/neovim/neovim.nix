ccf0780ac69b47950861dfdb0d6af3634a064973
Zozano <private@private.com>
2024-11-28 13:24:20 +1100

    2024-11-28_13:24:20

--- home-manager/modules/neovim/neovim.nix
+++ home-manager/modules/neovim/neovim.nix
@@ -9,12 +9,14 @@
   ...
 }:
 
-  programs.neovim = 
   let
     toLua = str: "lua << EOF\n${str}\nEOF\n";
     toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
-  in
+  in:
   {
+
+
+  programs.neovim = 
     enable = true;
 
     viAlias = true;
