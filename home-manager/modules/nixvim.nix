e4b55f73b5689941556226fb9c4613a2f4570f26
Zozano <private@private.com>
2024-11-28 14:00:35 +1100

    2024-11-28_14:00:35

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -4,6 +4,14 @@
   lib,
 ...
 }:
+
+let
+    nixvim = import (builtins.fetchGit {
+        url = "https://github.com/nix-community/nixvim";
+        # When using a different channel you can use `ref = "nixos-<version>"` to set it here
+    });
+in
+
 { 
   programs.nixvim.enable = true;
 
