f63bae513d8f4d32dfb327dc904fe4ec9bb5659a
Zozano <private@private.com>
2024-11-28 13:55:37 +1100

    2024-11-28_13:55:37

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -1,3 +1,9 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+};
     inputs.nixvim = {
         url = "github:nix-community/nixvim";
         # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
