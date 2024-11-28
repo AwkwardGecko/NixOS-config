ff4959531f3b55c61b36b55fd836417fbcc9fb61
Zozano <private@private.com>
2024-11-28 13:45:08 +1100

    2024-11-28_13:45:08

--- home-manager/modules/neovim/neovim.nix
+++ home-manager/modules/neovim/neovim.nix
@@ -96,13 +96,13 @@
       ${builtins.readFile ./options.lua}
     '';
 
-    # extraLuaConfig = ''
-    #   ${builtins.readFile ./nvim/options.lua}
-    #   ${builtins.readFile ./nvim/plugin/lsp.lua}
-    #   ${builtins.readFile ./nvim/plugin/cmp.lua}
-    #   ${builtins.readFile ./nvim/plugin/telescope.lua}
-    #   ${builtins.readFile ./nvim/plugin/treesitter.lua}
-    #   ${builtins.readFile ./nvim/plugin/other.lua}
-    # '';
+    extraLuaConfig = ''
+      ${builtins.readFile ./nvim/options.lua}
+      ${builtins.readFile ./nvim/plugin/lsp.lua}
+      ${builtins.readFile ./nvim/plugin/cmp.lua}
+      ${builtins.readFile ./nvim/plugin/telescope.lua}
+      ${builtins.readFile ./nvim/plugin/treesitter.lua}
+      ${builtins.readFile ./nvim/plugin/other.lua}
+    '';
   };
 }
