2562289bdc528e29e4105033a4ce8ea005b6ce93
Zozano <private@private.com>
2024-11-28 13:08:28 +1100

    2024-11-28_13:08:28

--- home-manager/modules/neovim/neovim.nix
+++ home-manager/modules/neovim/neovim.nix
@@ -9,88 +9,111 @@
   ...
 }:
 
-	let
-		toLua = str: "lua << EOF\n${str}\nEOF\n";
-		toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
-	in
-
-{
-  programs.neovim = {
-
-	enable = true;
-	defaultEditor = true;
-	viAlias = true;
-	vimAlias = true;
-	vimdiffAlias = true;
-
-
-
-	plugins = with pkgs.vimPlugins; [
-		
-		{ plugin = comment-nvim; # Vimjoyer recommended
-			config = toLua "require(\"Comment\").setup()";}
-
-		cmp_luasnip # Vimjoyer recommended
-		cmp-nvim-lsp # Vimjoyer recommended
-
-		{ plugin = gruvbox-material-nvim;
-			config = "gruvbox_material_background = 'hard'";}
-
-
-		{ plugin = gruvbox-nvim;
-			config = "colorscheme gruvbox";}
-
-		lualine-nvim
-
-		nvim-cmp # Vimjoyer recommended
-
-		nvim-tree-lua
-		nvim-web-devicons
-		
-		{ plugin = nvim-lspconfig; # Vimjoyer recommended
-			config = toLuaFile ./neovim/plugin/lsp.lua;}
-		
-		# nvim_luasnip # Vimjoyer recommended, not working?
-		friendly-snippets
-
-		telescope-nvim
-		telescope-fzf-native-nvim
-		
-		vim-nix
-
-		(nvim-treesitter.withPlugins (p: [
-			p.tree-sitter-nix
-			p.tree-sitter-vim
-			p.tree-sitter-bash
-			p.tree-sitter-fish
-			p.tree-sitter-lua
-			p.tree-sitter-python
-			p.tree-sitter-json
-		]))
-
-	];
-
-	# extraConfig = ''
-	#	foo
-	# '';
-
-	# 
-	extraLuaConfig = ''
-
-		${builtins.readFile ./neovim/options.lua}
-
-		require("ibl").setup()
-		vim.g.mapleader = ' '
-		vim.g.maplocalleader = ' '
-		vim.o.clipboard = 'unnamedplus'
-		vim.o.number = true
-		-- vim.o.relativenumber = true
-		vim.o.signcolumn = 'yes'
-		vim.o.tabstop = 4
-		vim.o.shiftwidth = 4
-		vim.o.updatetime = 300
-		vim.o.mouse = 'a'
-		vim.o.termguicolors = true
-	'';
+  nixpkgs = {
+    overlays = [
+      (final: prev: {
+        vimPlugins = prev.vimPlugins // {
+          own-onedark-nvim = prev.vimUtils.buildVimPlugin {
+            name = "onedark";
+            src = inputs.plugin-onedark;
+          };
+        };
+      })
+    ];
   };
-}
+
+  programs.neovim = 
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
+    enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    extraPackages = with pkgs; [
+      lua-language-server
+      rnix-lsp
+
+      xclip
+      wl-clipboard
+    ];
+
+    plugins = with pkgs.vimPlugins; [
+
+      {
+        plugin = nvim-lspconfig;
+        config = toLuaFile ./nvim/plugin/lsp.lua;
+      }
+
+      {
+        plugin = comment-nvim;
+        config = toLua "require(\"Comment\").setup()";
+      }
+
+      {
+        plugin = gruvbox-nvim;
+        config = "colorscheme gruvbox";
+      }
+
+      neodev-nvim
+
+      nvim-cmp 
+      {
+        plugin = nvim-cmp;
+        config = toLuaFile ./nvim/plugin/cmp.lua;
+      }
+
+      {
+        plugin = telescope-nvim;
+        config = toLuaFile ./nvim/plugin/telescope.lua;
+      }
+
+      telescope-fzf-native-nvim
+
+      cmp_luasnip
+      cmp-nvim-lsp
+
+      luasnip
+      friendly-snippets
+
+
+      lualine-nvim
+      nvim-web-devicons
+
+      {
+        plugin = (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-vim
+          p.tree-sitter-bash
+          p.tree-sitter-lua
+          p.tree-sitter-python
+          p.tree-sitter-json
+        ]));
+        config = toLuaFile ./nvim/plugin/treesitter.lua;
+      }
+
+      vim-nix
+
+      # {
+      #   plugin = vimPlugins.own-onedark-nvim;
+      #   config = "colorscheme onedark";
+      # }
+    ];
+
+    extraLuaConfig = ''
+      ${builtins.readFile ./nvim/options.lua}
+    '';
+
+    # extraLuaConfig = ''
+    #   ${builtins.readFile ./nvim/options.lua}
+    #   ${builtins.readFile ./nvim/plugin/lsp.lua}
+    #   ${builtins.readFile ./nvim/plugin/cmp.lua}
+    #   ${builtins.readFile ./nvim/plugin/telescope.lua}
+    #   ${builtins.readFile ./nvim/plugin/treesitter.lua}
+    #   ${builtins.readFile ./nvim/plugin/other.lua}
+    # '';
+  };
