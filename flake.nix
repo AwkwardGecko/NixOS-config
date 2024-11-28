b9c9843f8a27d7350e20877132339f959683b6eb
Zozano <private@private.com>
2024-11-28 13:39:13 +1100

    2024-11-28_13:39:13

--- flake.nix
+++ flake.nix
@@ -20,6 +20,10 @@
 		inputs.nixpkgs.follows = "nixpkgs";
 	};
 
+nixvim = {
+  url = "github:nix-community/nixvim";
+  inputs.nixpkgs.follows = "nixpkgs";
+  };
 
     plugin-onedark.url = "github:navarasu/onedark.nvim";
     plugin-onedark.flake = false;
