910f381adb7737a679dc18917ae9a5ddc160a869
Zozano <private@private.com>
2024-11-28 13:56:14 +1100

    2024-11-28_13:56:14

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -4,7 +4,8 @@
   lib,
   ...
 }:
-    inputs.nixvim = {
+{
+  inputs.nixvim = {
         url = "github:nix-community/nixvim";
         # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
         inputs.nixpkgs.follows = "nixpkgs";
