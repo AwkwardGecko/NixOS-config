f320e82a85d58ade56d1076c9f53ca92aef8a3dc
Zozano <private@private.com>
2024-11-28 12:49:29 +1100

    2024-11-28_12:49:29

--- home-manager/modules/neovim/neovim.nix
+++ home-manager/modules/neovim/neovim.nix
@@ -11,7 +11,7 @@
 
 	let
 		toLua = str: "lua << EOF\n${str}\nEOF\n";
-		toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n"
+		toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
 	in
 
 {
