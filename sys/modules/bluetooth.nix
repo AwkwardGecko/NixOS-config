7de49fd2a97e4262f842682857837aad4ba9d407
Zozano <private@private.com>
2024-11-29 16:55:24 +1100

    2024-11-29_16:55:24

--- sys/modules/bluetooth.nix
+++ sys/modules/bluetooth.nix
@@ -5,11 +5,29 @@
 
 	{ config, pkgs, lib, ... }: {
 
-      hardware.bluetooth = {
-        enable = true;
-        powerOnBoot = true;
-      };
-	  services.blueman.enable = true;
+
+   hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      package = pkgs.bluez5-experimental;
+      settings.Policy.AutoEnable = "true";
+      settings.General.Enable = "Source,Sink,Media,Socket";
+   };
+   services.blueman.enable = true;
+   environment.systemPackages = with pkgs; [
+        bluez5-experimental 
+        bluez-tools
+        bluez-alsa
+        bluetuith # can transfer files via OBEX
+      ];
+};}
+
+
+   #    hardware.bluetooth = {
+   #      enable = true;
+   #      powerOnBoot = true;
+   #    };
+	  # services.blueman.enable = true;
 
 	# environment.systemPackages = with pkgs; [
 	#	bluez
