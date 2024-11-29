a46ee54b277f76e25510136300b6712e238a3dcb
Zozano <private@private.com>
2024-11-29 16:46:18 +1100

    2024-11-29_16:46:18

--- sys/modules/bluetooth.nix
+++ sys/modules/bluetooth.nix
@@ -5,7 +5,8 @@
 
 	{ config, pkgs, lib, ... }: {
 
-	hardware.bluetooth.enable = true;
+      hardware.bluetooth.enable = true;
+      hardware.bluetooth.powerOnBoot = true;
 	services.blueman.enable = true;
 
 	# environment.systemPackages = with pkgs; [
