9bb3ceceae362083556d766856984cd918d94f40
Zozano <private@private.com>
2024-11-30 15:11:16 +1100

    2024-11-30_15:11:16

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -83,6 +83,7 @@ bootiso
 	gimp
     nixd # Language SP (LSP) server
     nixfmt-rfc-style # used to format styles - $ nixfmt <file>.nix
+    nettools
     #haskellPackages.cuda
     #haskellPackages.torch
     python310
