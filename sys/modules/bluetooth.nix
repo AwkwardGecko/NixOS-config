5eaf6c63d2e49e9ecdb6220fc14957c59e884995
Zozano <private@private.com>
2024-11-30 14:23:40 +1100

    2024-11-30_14:23:40

--- sys/modules/bluetooth.nix
+++ sys/modules/bluetooth.nix
@@ -9,13 +9,9 @@
    hardware.bluetooth = {
       enable = true;
         powerOnBoot = true;
-      settings = {
-          General = {
-            Experimental = true;
-          };
       };
     };   
-  services.blueman.enable = true;
+      #services.blueman.enable = true;
     }
 
    #    hardware.bluetooth = {
