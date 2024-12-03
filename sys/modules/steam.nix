cd08ae566ce13869d8b2b07b268fe06ea473af1d
Zozano <private@private.com>
2024-12-03 11:12:44 +1100

    2024-12-03_11:12:44

--- sys/modules/steam.nix
+++ sys/modules/steam.nix
@@ -7,7 +7,7 @@
 {
 	programs.steam = {
 		enable = true;
-		gamescopeSession.enable = true;
+        #gamescopeSession.enable = true;
 		remotePlay.openFirewall = true;
 		dedicatedServer.openFirewall = true;
 		localNetworkGameTransfers.openFirewall = true;
