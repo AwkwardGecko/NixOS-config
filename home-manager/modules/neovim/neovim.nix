10581c1b0ead48c8c27f2bfd9063f110312058f7
Zozano <private@private.com>
2024-12-04 10:39:10 +1100

    2024-12-04_10:39:10

--- home-manager/modules/neovim/neovim.nix
+++ home-manager/modules/neovim/neovim.nix
@@ -43,6 +43,10 @@
       }
 
       {
+        plugin = indent-blankline-nvim;
+        config = toLua "require("ibl").setup()"
+
+      {
         plugin = gruvbox-nvim;
         config = "colorscheme gruvbox";
       }
