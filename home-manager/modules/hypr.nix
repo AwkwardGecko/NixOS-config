fb52d08f43f5db9edbff5870aebabc47cb831c9b
Zozano <private@private.com>
2024-12-03 12:55:47 +1100

    2024-12-03_12:55:47

--- home-manager/modules/hypr.nix
+++ home-manager/modules/hypr.nix
@@ -161,15 +161,18 @@
 		];
 
 		bindl = [
-			",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-			",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-			",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          
+
 			",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
 			",XF86MonBrightnesUp, exec, brightnessctl s 10%+"
 			",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+
+            ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+			",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+			",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+
 			",XF86AudioNext, exec, playerctl next"
 			",XF86AudioPrev, exec, playerctl previous"
-			",XF86AudioPlay, exec, playerctl play-pause"
 			",XF86AudioPlay, exec, playerctl play-pause"
 		];
 
