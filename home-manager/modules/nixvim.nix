5d9ed1bef756e6a5aa282b17499987ac69f0358e
Zozano <private@private.com>
2024-12-04 10:30:41 +1100

    2024-12-04_10:30:41

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -5,14 +5,11 @@
 ...
 }:
 
-let
-    nixvim = import (builtins.fetchGit {
-        url = "https://github.com/nix-community/nixvim";
-        # When using a different channel you can use `ref = "nixos-<version>"` to set it here
-    });
-in
+{
+  programs.nixvim = {
+    enable = true;
 
-{ 
-  programs.nixvim.enable = true;
-
+    colorschemes.catppuccin.enable = true;
+    plugins.lualine.enable = true;
+  };
 }
