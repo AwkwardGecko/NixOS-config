24942d9ee8481ab1a1b52bf97fc128f14a961857
Zozano <private@private.com>
2024-12-06 10:23:33 +1100

    2024-12-06_10:23:33

--- sys/modules/autostart.nix
+++ sys/modules/autostart.nix
@@ -9,12 +9,13 @@
   ...
 }:
 {
-  systemd.user.services.zozano = {
+  #systemd.user.services.zozano = {
+  systemd.services.mount-server = {
     description = "mount-server";
-    serviceConfig.PassEnvironment = "DISPLAY";
+    #serviceConfig.PassEnvironment = "DISPLAY";
     script = ''
-      bash sshfs z-home@192.168.1.157:/ /server -oport=421
-      '';
+      bash "sshfs z-home@192.168.1.157:/ /server -oport=421"
+    '';
     wantedBy = [ "multi-user.target" ]; # starts after login
   };
 }
