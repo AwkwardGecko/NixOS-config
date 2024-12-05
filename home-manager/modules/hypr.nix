14366d5f958ff959f08e7eb1b6ed578937ee0e8a
Zozano <private@private.com>
2024-12-06 09:53:15 +1100

    2024-12-06_09:53:15

--- home-manager/modules/hypr.nix
+++ home-manager/modules/hypr.nix
@@ -37,10 +37,10 @@
 			#"pypr" #???
 		];
 
-        #env = [
-        #	"XCURSOR_SIZE,24"
-        #	"HYPRCURSOR_SIZE,24"
-          #];
+        env = [
+          "LIBVA_DRIVER_NAME,nvidia"
+          "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+        ];
         
 
 		#####################
