c40a4532c8c20885f28762cd3f03ededc9a7dc9e
Zozano <private@private.com>
2024-12-07 00:15:34 +1100

    2024-12-07_00:15:34

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -24,6 +24,7 @@
         modules-left = [
           "tray"
           "hyprland/workspaces"
+          "mpris"
           #"hyprland/window"
         ];
 
@@ -33,7 +34,6 @@
         ];
 
         modules-right = [
-          "mpris"
           "gamemode"
           "pulseaudio"
           "memory"
