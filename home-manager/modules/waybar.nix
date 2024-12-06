385f629dff0b8521066f66a1d28931f0e451200e
Zozano <private@private.com>
2024-12-07 00:10:21 +1100

    2024-12-07_00:10:21

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -150,7 +150,8 @@
         };
 
         mpris = {
-          format = "{player_icon} {title} - {artist} {position}";
+          format = "{player_icon} {title} - {artist} [{position}]";
+          interval = 1;
           format-paused = "{status_icon} <i>{dynamic}</i>";
           player-icons = {
             default = "▶";
