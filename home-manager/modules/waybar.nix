c2bfae04f45d8ed0cbc452bf8f9fba03b71b0101
Zozano <private@private.com>
2024-12-07 00:30:29 +1100

    2024-12-07_00:30:29

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -168,10 +168,10 @@
           icon-spacing = 0;
           format = "⏻ ";
           tooltip = true;
-          on-click = "shutdown now";
-          on-click-right = "reboot now";
-          #	"menu" = "on-click";
-          #	"menu-file" = "~/.dotfiles/home-manager/modules/waybar/power_menu.xml"; # Menu file in resources folder
+          #on-click = "shutdown now";
+          #on-click-right = "reboot now";
+          "menu" = "on-click";
+          "menu-file" = "~/.dotfiles/home-manager/modules/waybar/power_menu.xml"; # Menu file in resources folder
         };
 
         "menu-actions" = {
@@ -215,7 +215,49 @@
       	background: transparent;
       }
 
+      #tags {
+      	margin-top: 6px;
+      	margin-left: 12px;
+      	font-size: 4px;
+      	margin-bottom: 0px;
+      	border-radius: 10px;
+      	background: #161320;
+      	transition: none;
+      }
 
+      #tags button {
+      	transition: none;
+      	color: #B5E8E0;
+      	background: transparent;
+      	font-size: 16px;
+      	border-radius: 2px;
+      }
+
+      #tags button.occupied {
+      	transition: none;
+      	color: #F28FAD;
+      	background: transparent;
+      	font-size: 4px;
+      }
+
+      #tags button.focused {
+      	color: #ABE9B3;
+      	border-top: 2px solid #ABE9B3;
+      	border-bottom: 2px solid #ABE9B3;
+      }
+
+      #tags button:hover {
+      	transition: none;
+      	box-shadow: inherit;
+      	text-shadow: inherit;
+      	color: #FAE3B0;
+      	border-color: #E8A2AF;
+      	color: #E8A2AF;
+      }
+
+      #tags button.focused:hover {
+      	color: #E8A2AF;
+      }
 
       #mpris {
       	margin-top: 6px;
