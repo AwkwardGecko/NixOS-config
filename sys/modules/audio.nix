1b6c39e659f7abb45e0084031a4bfc24563eeaff
Zozano <private@private.com>
2024-12-06 11:18:32 +1100

    2024-12-06_11:18:32

--- sys/modules/audio.nix
+++ sys/modules/audio.nix
@@ -15,8 +15,8 @@
 
     enable = true;
     pulse.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    #alsa.enable = true;
+    #alsa.support32Bit = true;
     #jack.enable = true;
   };
 }
