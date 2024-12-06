ed02a3bfa22cc5b273b0737798ff1ec26363a8f9
Zozano <private@private.com>
2024-12-07 01:02:09 +1100

    2024-12-07_01:02:09

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -169,7 +169,7 @@
           format = "⏻ ";
           #tooltip = true;
           on-click = "shutdown now";
-          on-click-right = "sudo reboot now";
+          on-click-right = "reboot";
           #on-click = "menu";
           #menu = "on-click";
           #menu-file = "~/.local/share/applications/power_menu.xml"; # Menu file in resources folder
