f752a6b5f0f920219d5686952dee7583537199f7
Zozano <private@private.com>
2024-12-06 21:28:35 +1100

    2024-12-06_21:28:35

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -52,9 +52,10 @@
 
   environment.sessionVariables = {
     XDG_DATA_DIRS= [
-      /usr/share
-      /var/lib/flatpak/exports/share
-      $HOME/.local/share/flatpak/exports/share
+      "$XDG_DATA_DIRS"
+      "/usr/share"
+      "/var/lib/flatpak/exports/share"
+      "$HOME/.local/share/flatpak/exports/share"
     ];
   };
 
