dcbb6b4ba32ba642baef92a4d98e7987bdc762c6
Zozano <private@private.com>
2024-12-17 15:04:44 +1100

    2024-12-17_15:04:44

--- home-manager/modules/hypr.nix
+++ home-manager/modules/hypr.nix
@@ -195,7 +195,7 @@
       "workspace 10,title:(Fallout)"
       "workspace 11,title:(Valheim)"
       "workspace 12,title:(Shovel Knight)"
-      "workspace 13,title:(Payday)"
+      "workspace 13,title:(PAYDAY)"
       "suppressevent maximize, class:.*"
       "nofocus,class:^$,title:^$,wayland:1,floating:1,fullscreen:0,pinned:0"
     ];
