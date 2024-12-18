5f5c983981c59251cf47da6b94b705a9d62c12f0
Zozano <private@private.com>
2024-12-18 18:34:13 +1100

    2024-12-18_18:34:13

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -56,6 +56,7 @@
 
   home.packages = with pkgs; [
 	baobab # disk usage analyzer
+    bing-wall
     #blueberry
     clementine
     cliphist
---
+++ jellyfin.nix
@@ -0,0 +1,9 @@
+Scheduled Tasks
+  Application
+    Update Plugins = 3am
+
+  Intro Skipper
+    Clean up cache = 3am
+    Analyze Media Segements = 3am
+
+  Auto Normalization = 3am
