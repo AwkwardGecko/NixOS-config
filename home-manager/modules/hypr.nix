cfccb28cc47006a8cddaa2fee826303f7418a200
Zozano <private@private.com>
2025-01-04 11:54:01 +1100

    2025-01-04_11:54:01

--- home-manager/modules/hypr.nix
+++ home-manager/modules/hypr.nix
@@ -132,7 +132,7 @@
       "$mainMod, R, exec, $menu"
       "$mainMod, P, pseudo,"
       "$mainMod, J, togglesplit,"
-      ", F9, exec, bash kill deadcells"
+      "$mainMod, F9, exec, bash kill deadcells"
 
       "$mainMod, left, movefocus, l"
       "$mainMod, right, movefocus, r"
