dc111deda1e07d346d693b9ff11f609c2d036659
Zozano <private@private.com>
2025-01-10 08:56:31 +1100

    2025-01-10_08:56:31

--- home-manager/modules/hypr.nix
+++ home-manager/modules/hypr.nix
@@ -197,6 +197,7 @@
       "workspace 11,title:(Valheim)"
       "workspace 12,title:(Shovel Knight)"
       "workspace 13,title:(PAYDAY)"
+      "workspace 14,title:(Dead Cells)"
       "suppressevent maximize, class:.*"
       "nofocus,class:^$,title:^$,wayland:1,floating:1,fullscreen:0,pinned:0"
     ];
