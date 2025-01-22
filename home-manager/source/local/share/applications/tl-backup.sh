57a8d5161e8a9d955e89e8cf9fa83c39934ac52c
Zozano <private@private.com>
2025-01-22 23:23:47 +1100

    2025-01-22_23:23:47

--- home-manager/source/local/share/applications/tl-backup.sh
+++ home-manager/source/local/share/applications/tl-backup.sh
@@ -2,6 +2,6 @@
 
 cd "/steam/SteamLibrary/steamapps/compatdata/41500/pfx/drive_c/users/steamuser/AppData/Roaming/runic games/"
 
-mv torchlight.tar.gz old-backups/torchlight${date -d "today" +"%Y-%m-%d_%H-%M").tar.gz"
+mv torchlight.tar.gz old-backups/torchlight$(date -d "today" +"%Y-%m-%d_%H-%M").tar.gz"
 
 tar -czvf torchlight.tar.gz torchlight/
