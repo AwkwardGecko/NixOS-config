1c4de666ef9df654742c91d53fa65c265c8e4fe3
Zozano <private@private.com>
2025-02-01 10:53:36 +1100

    2025-02-01_10:53:36

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -68,6 +68,17 @@
   ];
 
 
+system.autoUpgrade = {
+  enable = true;
+  flake = inputs.self.outPath;
+  flags = [
+    "--update-input"
+    "nixpkgs"
+    "-L" # print build logs
+  ];
+  dates = "02:00";
+  randomizedDelaySec = "45min";
+};
 
 
   environment.systemPackages = with pkgs; [
