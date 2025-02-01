d09f094d65e51498630b919bb52ef463291ed118
Zozano <private@private.com>
2025-02-02 10:02:48 +1100

    2025-02-02_10:02:48

--- sys/modules/nvidia.nix
+++ sys/modules/nvidia.nix
@@ -18,7 +18,7 @@
       nvidiaSettings = true;
       powerManagement.enable = false;
       powerManagement.finegrained = false;
-      package = config.boot.kernelPackages.nvidiaPackages.production;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
 
     graphics = {
