77fc276111c6f5641b9d4770acb450cebfb11436
Zozano <private@private.com>
2025-02-07 11:36:46 +1100

    2025-02-07_11:36:46

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ cd ~/.dotfiles
 git add *
 git commit -m "$(date '+%F_%H:%M:%S')"
 git push github main
-home-manager switch -b backup --flake /home/zozano/.dotfiles/#
+home-manager switch -b backup --flake /home/zozano/.dotfiles/#zozano
 #sudo nixos-rebuild switch --upgrade --flake ~/.dotfiles/
