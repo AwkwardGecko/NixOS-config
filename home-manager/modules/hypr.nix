e0e60683e7ee3471457bf24134567299acf295ce
Zozano <private@private.com>
2025-02-07 14:16:29 +1100

    2025-02-07_14:16:29

--- home-manager/modules/hypr.nix
+++ home-manager/modules/hypr.nix
@@ -61,8 +61,8 @@
       gaps_in = 5;
       gaps_out = 10;
       border_size = 2;
-      "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-      "col.inactive_border" = "rgba(595959aa)";
+      "active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+      "inactive_border" = "rgba(595959aa)";
       resize_on_border = false;
       allow_tearing = false;
       layout = "dwindle";
@@ -74,9 +74,11 @@
       active_opacity = 1.0;
       inactive_opacity = 1.0;
       drop_shadow = true;
-      shadow_range = 4;
-      shadow_render_power = 3;
-      "col.shadow" = "rgba(1a1a1aee)";
+      shadow = {
+          range = 4;
+          render_power = 3;
+          color= "rgba(1a1a1aee)";
+      }
 
       blur = {
         enabled = true;
--- z-nixos/configuration.nix
+++ z-nixos/configuration.nix
@@ -47,8 +47,6 @@
     "SDL_ttf-2.0.11"    # test removing this soon
   ];
 
-  programs.cavalier.enable = true;
-
   security.doas.enable = true;
 
   programs.ssh.startAgent = true;
