9c8f211f0ae4ef609e86ea9ef36ffca6a7e77c39
Zozano <private@private.com>
2025-02-20 19:28:46 +1100

    2025-02-20_19:28:46

--- z-nixos/modules/nvidia.nix
+++ z-nixos/modules/nvidia.nix
@@ -30,7 +30,7 @@
     };
 
 
-  environment.variables = {
+  system.environment.variables = {
     GBM_BACKEND = "nvidia-drm";
     LIBVA_DRIVER_NAME = "nvidia";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
