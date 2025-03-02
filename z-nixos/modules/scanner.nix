69ec4fc5ac6d8f9d0e721d0a359782be277ff05f
Zozano <private@private.com>
2025-03-02 13:22:16 +1100

    2025-03-02_13:22:16

--- z-nixos/modules/scanner.nix
+++ z-nixos/modules/scanner.nix
@@ -1,12 +1,25 @@
 { config, pkgs, lib, ... }:
 
 {
-  hardware.sane.enable = true;
+  hardware.sane = {
+    enable = true;
+    
+    extraBackends = [
+      pkgs.utushi
+    ];
+  };
+
+  services.udev.packages = [
+      pkgs.utushi
+  ];
+
   users.users.zozano.extraGroups = [
     "scanner"
     "lp"
   ];
+
   environment.systemPackages = with pkgs; [
     simple-scan
   ];
+
 }
