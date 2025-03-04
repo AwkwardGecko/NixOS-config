689c8c9189e1219dac9cdf0feeab84da6781bc0c
Zozano <private@private.com>
2025-03-05 10:59:55 +1100

    2025-03-05_10:59:55

--- home-manager/modules/hypr.nix
+++ home-manager/modules/hypr.nix
@@ -204,6 +204,7 @@
       "workspace 15,title:(Torchlight)"
       "workspace 16,title:(Rocket League)"
       "workspace 17,title:(Cyberpunk2077)"
+      "workspace 18,title:(Grand Theft Auto)"
       "suppressevent maximize, class:.*"
       "nofocus,class:^$,title:^$,wayland:1,floating:1,fullscreen:0,pinned:0"
     ];
