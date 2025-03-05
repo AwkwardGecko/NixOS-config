af706e955f4a2f7a0d888c3f835a6376bf12dbad
Zozano <private@private.com>
2025-03-05 11:31:42 +1100

    2025-03-05_11:31:42

--- z-nixos/modules/bluetooth.nix
+++ z-nixos/modules/bluetooth.nix
@@ -24,7 +24,7 @@
     settings.General = {
       Privacy = "device";
       JustWorksRepairing = "always";
-      Class = "0x000000";
+      Class = "0x000100";
       FastConnectable = "true";
       # ControllerMode = "dual";
     };
