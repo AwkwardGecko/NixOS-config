eeffaf13338165f5cc1125042cef5b4eb5278c7f
Zozano <private@private.com>
2025-03-17 10:50:15 +1100

    2025-03-17_10:50:14

--- z-nixos/configuration.nix
+++ z-nixos/configuration.nix
@@ -53,9 +53,12 @@
   programs.npm.enable = true;
 
 
-  networking.firewall.allowedTCPPorts = [
-    8080 # Open-WebUI
-  ];
+  networking.firewall = { 
+    allowedTCPPorts = [ 8080 # Open-WebUI 
+    ];
+    extraCommands = ''
+      iptables -A nixos-fw -p tcp --source 192.168.1.0/24 --dport 8080 -j nixos-fw-accept '';
+  };
 
   services.open-webui.enable = true;
   services.ollama = {
