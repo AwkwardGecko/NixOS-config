cefb02816646719e7da7c43bcc911dca47a00985
Zozano <private@private.com>
2025-03-18 14:44:55 +1100

    2025-03-18_14:44:55

--- system-update.sh
+++ system-update.sh
@@ -6,4 +6,3 @@ git add *
 git commit -m "$(date '+%F_%H:%M:%S')"
 git push github main
 sudo nixos-rebuild switch --upgrade --flake /home/zozano/.dotfiles/#z-nixos
-home-manager switch -b backup --flake /home/zozano/.dotfiles/#
