8652b8076c2341037a03549a2a26d988d57841b0
Zozano <private@private.com>
2025-03-18 12:17:52 +1100

    2025-03-18_12:17:52

--- z-nixos/modules/ssh.nix
+++ z-nixos/modules/ssh.nix
@@ -9,9 +9,18 @@
   ...
 }:
 {
-  users.users.zozano.openssh.authorizedKeys.keyFiles = [
-    /home/zozano/.ssh
-  ];
+  users.users.zozano = {
+    openssh.authorizedKeys.keys = [
+  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMw7Bpl8lnXMVQF6dZPJA3qHRDgIMwcmTowzVigWWZ2E your_email@example.com"
+    ];
+  };
 
-
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+      KbdInteractiveAuthentication = false;
+    };
+  };
 }
