c7608feee5efc609354186a913a8969f011e148d
Zozano <private@private.com>
2025-03-19 13:03:48 +1100

    2025-03-19_13:03:48

---
+++ flake-modules/inputs.nix
@@ -0,0 +1,23 @@
+{ ... }:
+
+{
+  inputs = {
+    stylix.url = "github:danth/stylix";
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
+    nix-colors.url = "github:misterio77/nix-colors";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    aagl = {
+      url = "github:ezKEa/aagl-gtk-on-nix";
+      inputs.nixpkgs.follows = "nixpkgs"; 
+    };
+    plugin-onedark.url = "github:navarasu/onedark.nvim";
+    plugin-onedark.flake = false;
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+}
