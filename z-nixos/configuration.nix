e0c67b07210f13f6892b6b0b1bfc55bccc042bc2
Zozano <private@private.com>
2025-03-27 10:59:34 +1100

    2025-03-27_10:59:34

--- z-nixos/configuration.nix
+++ z-nixos/configuration.nix
@@ -96,7 +96,7 @@
     ./modules/docker.nix
     ./modules/filesystem.nix
     ./modules/firefox.nix
-    ./modules/fonts.nix
+    #./modules/fonts.nix
     ./modules/gamemode.nix
     ./modules/hypr.nix
     ./modules/internationalisation.nix
