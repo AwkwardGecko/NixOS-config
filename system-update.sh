8022370a495f4d57734684d1e45579d0ba7fd22f
Zozano <private@private.com>
2025-03-27 11:54:27 +1100

    2025-03-27_11:54:27

--- system-update.sh
+++ system-update.sh
@@ -3,7 +3,6 @@
 cd /home/zozano/.dotfiles
 nix flake update
 sudo nixos-rebuild switch --upgrade --flake /home/zozano/.dotfiles/#z-nixos
-fc-cache -f -v
 git add *
 git commit -m "$(date '+%F_%H:%M:%S')"
 git push github main
