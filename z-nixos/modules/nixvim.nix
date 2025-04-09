b075d1eff2c06128737bfad3f3cc2e07a80ebd39
Zozano <private@private.com>
2025-04-09 12:04:35 +1000

    2025-04-09_12:04:35

--- z-nixos/modules/nixvim.nix
+++ z-nixos/modules/nixvim.nix
@@ -1,21 +1,33 @@
 { pkgs, ... }: {
   programs.nixvim = {
     enable = true;
-    colorschemes.gruvbox.enable = true;
+    colorschemes.gruvbox.enable = true;  # Gruvbox colorscheme
 
     plugins = {
-      lualine.enable = true;
-      treesitter.enable = true;
+      lualine.enable = true;               # Statusline plugin
+      treesitter.enable = true;            # Advanced syntax highlighting
+      telescope.enable = true;             # Fuzzy finder for files and more
+      nvim-cmp.enable = true;              # Autocompletion plugin
+      nvim-lspconfig.enable = true;        # Language Server Protocol integration
+      bufferline.enable = true;            # Buffer tabline for better navigation
+      gitsigns.enable = true;              # Git integration in the editor
+      comment.enable = true;               # Easy commenting of code
+      autopairs.enable = true;             # Automatic pairing of parentheses and brackets
+      indent-blankline.enable = true;      # Visual indentation guides
+      nerdtree.enable = true;              # File explorer plugin
+      fugitive.enable = true;              # Git commands inside Neovim
+      dashboard-nvim.enable = true;        # Start screen with shortcuts and session info
     };
 
     opts = {
-      number = true;               # Enable line numbers
-      relativenumber = true;       # Enable relative line numbers
-      shiftwidth = 2;              # Set indentation width to 2 spaces
-      tabstop = 2;                 # Set tab width to 2 spaces
-      expandtab = true;            # Use spaces instead of tabs
-      smartindent = true;          # Enable smart indentation
-      autoindent = true;           # Enable automatic indentation
+      number = true;                       # Show absolute line numbers
+      relativenumber = true;               # Show relative line numbers
+      shiftwidth = 2;                      # Set indentation width to 2 spaces
+      tabstop = 2;                         # Set tab width to 2 spaces
+      expandtab = true;                    # Use spaces instead of tabs
+      smartindent = true;                  # Enable smart indentation
+      autoindent = true;                   # Enable automatic indentation
     };
   };
 }
+
