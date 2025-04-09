a3690ad463639f5f08258dcf85ba45fa2b59d505
Zozano <private@private.com>
2025-04-09 11:47:16 +1000

    2025-04-09_11:47:16

--- z-nixos/modules/nixvim.nix
+++ z-nixos/modules/nixvim.nix
@@ -1,21 +1,27 @@
 { pkgs, ... }: {
   programs.nixvim = {
     enable = true;
-    colorscheme = "gruvbox";
+    colorscheme = "gruvbox";  # Set the colorscheme to gruvbox
+
     plugins = {
       lualine.enable = true;
       treesitter.enable = true;
-	plugin = [
-	  {
-          name = "gruvbox-community/gruvbox";
-          type = "git";
-          url = "https://github.com/gruvbox-community/gruvbox";
-          }
-	];
     };
+
     extraConfigVim = ''
+      " Use vim-plug for plugin management
+      call plug#begin('~/.vim/plugged')
+
+      " Add gruvbox plugin
+      Plug 'gruvbox-community/gruvbox'
+
+      " End plugin installation
+      call plug#end()
+
+      " Additional Vim settings
       set number
       set relativenumber
+      colorscheme gruvbox  # Ensure the colorscheme is set correctly
     '';
   };
 }
