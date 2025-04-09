f1a31590b928d371cb3afd34ca3f948f6d943b6d
Zozano <private@private.com>
2025-04-09 14:20:13 +1000

    2025-04-09_14:20:13

--- z-nixos/configuration.nix
+++ z-nixos/configuration.nix
@@ -109,128 +109,125 @@
 
   environment.systemPackages = with pkgs; [
 
-    alsa-lib
-    atk
-    bazel
-    binutils_nogold
-    #bluez-alsa
-    #bluez-experimental
-    #bluez-tools
-    bootiso
-    brave
-    #btmon
-    btrfs-progs
-    cairo
-    cargo
-    cargo-auditable-cargo-wrapper
-    cargo-c
-    cargo-deb
-    cargo-rr
-    conda
-    clinfo
-    crane
-    crane
-    crate2nix
-    cups
-    dbus
-    dcap
-    digikam
-    docker
-    docker-compose
-    evince # document viewer
-    evtest # outputs key and button codes
-    expat
-    ffmpeg
-    gcc
-    gcc-unwrapped # for stable diffusion
-    gimp
-    git
-    glib
-    glibc
-    glibc_memusage
-    gperftools # for stable diffusion
-    gnome-calculator
-    gnome-disk-utility
-    gparted
-    gperftools
-    gtk3
-    gtkd
-    gwe
-    icu
-    iproute2
-    koboldcpp
-    libGL
-    libdrm
-    libglvnd # for stable diffusion
-    libheif
-    libheif.out
-    libpcap # 2025-02-19 for star rail relic scorer: Fribbels Honkai Star Rail Optimizer
-    libsecret
-    libxkbcommon
-    libz
-    lld
-    llvmPackages.bintools
-    lutris
-    monero-gui
-    mesa
-    nettools
-    nixd # Language SP (LSP) server
-    nixfmt-rfc-style # used to format styles - $ nixfmt <file>.nix
-    nodePackages.nodejs
-    nspr
-    nss
-    nvtopPackages.nvidia
-    onlyoffice-bin
-    openssl # For updater / reliquary-archiver
-    ostree # package for flatpak
-    pango
-    parted
-    pavucontrol
-    pcapfix
-    pkg-config # for reliquary-archiver
-    playonlinux
-    protobuf
-    pxattr # allows execution of .app files
-    pyenv
-    python311
-    python311Packages.pip
-    python311Packages.pyasyncore
-    python311Packages.xattr
-    python312Packages.libpcap
-    qbittorrent
-    rclone
-    rpm-ostree # package for flatpak
-    rustc
-    rustc
-    rustup # for reliquary-archiver
-    rustup-toolchain-install-master
-    sc-controller # steam controller support
-    signal-desktop
-    smartmontools
-    sqlite # possible dependency for cargo
-    sshfs
-    stdenv
-    strawberry
-    udev
-    unigine-superposition
-    unixtools.ifconfig
-    upower # possible dependency for vivaldi
-    uv
-    vivaldi
-    vlc
-    vulkan-loader
-    vulkan-tools
-    wget
-    wine
-    xorg.libX11
-    xorg.libXcomposite
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXrandr
-    xorg.libxcb
-    yt-dlp # download YouTube videos
-    zlib # for stable diffusion / reliquary launcher
+     alsa-lib                   # ALSA sound library
+    atk                         # Accessibility toolkit (GNOME dependencies)
+    bazel                       # Google's build tool (used for TensorFlow etc.)
+    binutils_nogold             # Binutils without the gold linker
+    # bluez-alsa                # ALSA backend for Bluetooth audio
+    # bluez-experimental       # Experimental Bluetooth stack features
+    # bluez-tools              # Utilities for Bluetooth
+    bootiso                     # Create bootable USB drives from ISO
+    brave                       # Web browser
+    # btmon                    # Bluetooth monitoring/debugging
+    btrfs-progs                 # Btrfs filesystem tools
+    cairo                       # 2D graphics library
+    cargo                       # Rust package manager
+    cargo-auditable-cargo-wrapper  # Adds audit metadata to Rust binaries
+    cargo-c                     # Build C-style shared libs from Rust
+    cargo-deb                   # Generate .deb packages from Rust projects
+    cargo-rr                    # Run Rust programs under rr debugger
+    clinfo                      # Lists OpenCL devices
+    conda                       # Python environment manager
+    crate2nix                   # Converts Cargo.toml into Nix expressions
+    crane                       # Nix-native Rust build system (like crate2nix)
+    cups                        # Printing system
+    dbus                        # IPC system used by desktop apps
+    dcap                        # Data Center Attestation Primitives (SGX stuff)
+    digikam                     # Photo management software
+    docker                      # Container engine
+    docker-compose              # Define & run multi-container apps with Docker
+    evince                      # Document viewer
+    evtest                      # Reads input events (debugging input devices)
+    expat                       # XML parsing library
+    ffmpeg                      # Video and audio processing tool
+    gcc                         # GNU Compiler Collection
+    gcc-unwrapped               # Unwrapped GCC (required by some projects like Stable Diffusion)
+    gimp                        # Image editing software
+    git                         # Version control
+    glib                        # Low-level GNOME core library
+    glibc                       # GNU C Library
+    glibc_memusage              # Tracks memory usage of programs
+    gnome-calculator            # Calculator app
+    gnome-disk-utility          # Disk management GUI
+    gparted                     # Partitioning tool
+    gperftools                  # Google Performance Tools (used by Stable Diffusion)
+    gtk3                        # GNOME GUI toolkit v3
+    gtkd                        # D bindings for GTK
+    gwe                         # GreenWithEnvy - NVIDIA GPU control GUI
+    icu                         # Unicode support libraries
+    iproute2                    # Network tools (replacement for net-tools)
+    koboldcpp                   # Local LLM interface for KoboldAI
+    libGL                       # OpenGL library
+    libdrm                      # Direct Rendering Manager (graphics stuff)
+    libglvnd                    # OpenGL Vendor-Neutral Dispatch library (Stable Diffusion dependency)
+    libheif                    # HEIF image support
+    libheif.out                # Possibly accidental duplicate; same purpose
+    libpcap                     # Packet capture library (used in Fribbels Honkai Star Rail Optimizer)
+    libsecret                   # Secret storage (GNOME keyring)
+    libxkbcommon                # Keyboard layout handling (Wayland/X)
+    libz                        # Compression library (zlib)
+    lld                         # LLVM linker
+    llvmPackages.bintools      # LLVM toolchain binaries (e.g., ar, nm)
+    #lutris                      # Game manager (especially for Wine games)
+    mesa                        # Open-source graphics drivers
+    monero-gui                  # GUI wallet for Monero cryptocurrency
+    nettools                    # Old-school network tools (ifconfig, etc.)
+    nixd                        # Nix language server (LSP)
+    nixfmt-rfc-style            # Nix formatter
+    nodePackages.nodejs         # Node.js runtime
+    nspr                        # Netscape Portable Runtime (used by Firefox, etc.)
+    nss                         # Network Security Services
+    nvtopPackages.nvidia        # GPU usage monitor for NVIDIA
+    onlyoffice-bin              # Office suite
+    openssl                     # TLS/SSL support (used by reliquary-archiver and other tools)
+    ostree                      # Backend for Flatpak, versioned filesystem
+    pango                       # Text rendering library
+    parted                      # Partitioning tool
+    pavucontrol                 # PulseAudio volume control GUI
+    pcapfix                     # Repairs broken .pcap files
+    pkg-config                  # Finds C libraries (used by reliquary-archiver)
+    playonlinux                 # Wine frontend for installing Windows games
+    protobuf                    # Google's Protocol Buffers (serialization)
+    pxattr                      # Manipulate extended file attributes (.app file exec support)
+    pyenv                       # Python version manager
+    python311                   # Python 3.11 interpreter
+    python311Packages.pip       # Python 3.11 pip installer
+    python311Packages.pyasyncore # Async networking module for Python 3.11
+    python311Packages.xattr     # Python module for extended attributes
+    python312Packages.libpcap   # Python bindings for libpcap (next-gen testing?)
+    qbittorrent                 # BitTorrent client
+    rclone                      # Sync with cloud storage
+    rpm-ostree                  # System image manager (used with Flatpak)
+    #rustc                       # Rust compiler
+    rustup                      # Rust toolchain manager (used by reliquary-archiver)
+    rustup-toolchain-install-master # Install Rust toolchains from master
+    sc-controller               # Steam controller driver + GUI
+    signal-desktop              # Encrypted messaging desktop app
+    smartmontools               # Monitor hard drive health (S.M.A.R.T.)
+    sqlite                      # Embedded SQL database engine
+    sshfs                       # Mount remote filesystems over SSH
+    stdenv                      # Nix standard environment
+    strawberry                  # Music player
+    udev                        # Device manager for the Linux kernel
+    unigine-superposition       # GPU benchmarking tool
+    unixtools.ifconfig          # Brings back `ifconfig`
+    upower                      # Power management daemon (dependency for Vivaldi maybe)
+    uv                          # Fast Python package manager
+    vivaldi                     # Web browser
+    vlc                         # Media player
+    vulkan-loader               # Vulkan runtime loader
+    vulkan-tools                # Vulkan utilities like `vulkaninfo`
+    wget                        # File downloader
+    wine                        # Windows compatibility layer
+    xorg.libX11                 # Core X11 library
+    xorg.libXcomposite          # Compositing support for X11
+    xorg.libXdamage             # Damage tracking for X11
+    xorg.libXext                # Misc X11 extensions
+    xorg.libXfixes              # X11 fixes extension
+    xorg.libXrandr              # X11 RandR extension (screen resizing)
+    xorg.libxcb                 # X protocol C-language Binding
+    yt-dlp                      # YouTube downloader
+    zlib                        # Compression lib (used by Stable Diffusion & reliquary launcher)
   ];
 
 }
