62a37a4ad417cf1577a4f519539cb0dd7beb39ad
Zozano <private@private.com>
2025-04-09 11:51:44 +1000

    2025-04-09_11:51:44

--- z-nixos/modules/nixvim.nix
+++ z-nixos/modules/nixvim.nix
@@ -4,26 +4,17 @@
     colorscheme = "gruvbox";  # Set the colorscheme to gruvbox
 
     plugins = {
+      # Enable lualine and treesitter as plugins
       lualine.enable = true;
       treesitter.enable = true;
     };
 
+    # Declare the plugins explicitly using vimPlugins from Nix
+    vimPlugins = with pkgs.vimPlugins; [
+      gruvbox-community.gruvbox  # Add the gruvbox plugin
+    ];
+
     extraConfigVim = ''
-      " Ensure packer is installed
-      if empty(glob('~/.local/share/nvim/site/pack/packer/start/packer.nvim'))
-        silent !git clone --depth 1 https://github.com/wbthomason/packer.nvim \
-            ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-      endif
-
-      " Initialize packer
-      lua << EOF
-        require('packer').startup(function(use)
-          use 'gruvbox-community/gruvbox'  -- Add the gruvbox plugin
-          use 'nvim-lualine/lualine.nvim'  -- lualine plugin
-          use 'nvim-treesitter/nvim-treesitter'  -- treesitter plugin
-        end)
-      EOF
-
       " Enable line numbers
       set number           " Show absolute line numbers
       set relativenumber   " Show relative line numbers
@@ -35,8 +26,8 @@
       set smartindent      " Enable smart indentation
       set autoindent       " Enable automatic indentation
 
-      " Additional Vim settings
-      colorscheme gruvbox  # Ensure the colorscheme is set correctly
+      " Ensure the colorscheme is applied correctly
+      colorscheme gruvbox  # Set the gruvbox colorscheme
     '';
   };
 }
