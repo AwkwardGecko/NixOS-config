4ba9414cc6cc1331b09267126fb9c39e067ad134
Zozano <private@private.com>
2025-04-09 11:58:21 +1000

    2025-04-09_11:58:21

--- z-nixos/modules/nixvim.nix
+++ z-nixos/modules/nixvim.nix
@@ -4,25 +4,17 @@
     colorschemes.gruvbox.enable = true;
 
     plugins = {
-      # Enable lualine and treesitter as plugins
       lualine.enable = true;
       treesitter.enable = true;
     };
 
-
-    extraConfigVim = ''
-      " Enable line numbers
-      set number           " Show absolute line numbers
-      set relativenumber   " Show relative line numbers
-
-      " Enable line indentation
-      set tabstop=4        " Set tab width to 4 spaces
-      set shiftwidth=4     " Indentation width
-      set expandtab        " Use spaces instead of tabs
-      set smartindent      " Enable smart indentation
-      set autoindent       " Enable automatic indentation
-
-    '';
-  };
+    opts = {
+      number = true;               # Enable line numbers
+      relativenumber = true;       # Enable relative line numbers
+      shiftwidth = 2;              # Set indentation width to 2 spaces
+      tabstop = 2;                 # Set tab width to 2 spaces
+      expandtab = true;            # Use spaces instead of tabs
+      smartindent = true;          # Enable smart indentation
+      autoindent = true;           # Enable automatic indentation
+    };
 }
-
