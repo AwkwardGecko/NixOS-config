95668ac176cff2aec49c9fd0b93cbd3b23f03db2
Zozano <private@private.com>
2025-04-10 12:28:42 +1000

    2025-04-10_12:28:42

--- z-nixos/modules/monero.nix
+++ z-nixos/modules/monero.nix
@@ -5,9 +5,9 @@
   services.monero = {
     enable = true;
     dataDir = "/steam/Monero";
-    #    mining.enable = true;
-    #mining.wallet = "your-wallet-address";  # Replace with your Monero wallet address
-    #mining.threads = 6;  # Adjust based on how many threads you want to allocate for mining
+    mining.enable = true;
+    mining.wallet = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";  # Replace with your Monero wallet address
+    mining.threads = 6;  # Adjust based on how many threads you want to allocate for mining
   };
 
   # Install XMRig and configure it
@@ -15,8 +15,6 @@
     xmrig  # Install XMRig mining software
     monero-cli # checking sync speed 
   ];
-
-
 
   # Optional: Power management to optimize energy usage during mining
   powerManagement.cpuFreqGovernor = "powersave";  # Use a low power governor when not actively using the CPU
