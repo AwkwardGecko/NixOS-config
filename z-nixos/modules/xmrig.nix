b1c487f70f36c43378447efb49c0016a65ee0d2f
Zozano <private@private.com>
2025-04-10 17:22:07 +1000

    2025-04-10_17:22:07

--- z-nixos/modules/xmrig.nix
+++ z-nixos/modules/xmrig.nix
@@ -4,7 +4,7 @@ let
   xmrigConfigJSON = builtins.toJSON {
     pools = [
       {
-        url = "monero-asia1.nanopool.org:14444";
+        url = "monero-eu1.nanopool.org:14443";
         user = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";
         pass = "x"; # optional password
         tls = true;
