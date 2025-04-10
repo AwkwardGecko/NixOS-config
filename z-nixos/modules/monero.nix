ca639306d8a6d322e7f7b858455064a38734b494
Zozano <private@private.com>
2025-04-10 17:38:17 +1000

    2025-04-10_17:38:17

--- z-nixos/modules/monero.nix
+++ z-nixos/modules/monero.nix
@@ -5,9 +5,9 @@
   services.monero = {
     enable = true;
     dataDir = "/steam/Monero";
-    mining.enable = true;
-    mining.address = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";  # Replace with your Monero wallet address
-    mining.threads = 6;  # Adjust based on how many threads you want to allocate for mining
+    #mining.enable = true;
+    #mining.address = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";  # Replace with your Monero wallet address
+    #mining.threads = 6;  # Adjust based on how many threads you want to allocate for mining
   };
 
 }
