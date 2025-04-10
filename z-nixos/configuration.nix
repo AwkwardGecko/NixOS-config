92aa23a024a0407f5d3d4bc80cd9d28889075bc6
Zozano <private@private.com>
2025-04-10 21:48:15 +1000

    2025-04-10_21:48:15

--- z-nixos/configuration.nix
+++ z-nixos/configuration.nix
@@ -49,6 +49,8 @@
   networking.networkmanager.enable = true;
   networking.interfaces.enp10s0.macAddress = "04:42:1A:A7:FD:1F";
 
+  networking.nameserver = [ "1.1.1.1" "8.8.8.8"]
+
   networking.firewall.allowedTCPPorts = [ 
     18080 # monero
     18081 # monero
