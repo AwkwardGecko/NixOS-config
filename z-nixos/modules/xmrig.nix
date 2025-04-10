bf4eb394706924bf9860f67b8978b1cc1888213f
Zozano <private@private.com>
2025-04-10 17:41:53 +1000

    2025-04-10_17:41:53

--- z-nixos/modules/xmrig.nix
+++ z-nixos/modules/xmrig.nix
@@ -12,7 +12,7 @@
      
       pools = [
         {
-          url = "pool-aus.supportxmr.com:443";
+          url = "xmr-eu1.nanopool.org:14444";
           user = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";
           keepalive = true;
           tls = true;
