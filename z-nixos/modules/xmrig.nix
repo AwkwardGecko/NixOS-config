38aeed13ec2d4c69de463e181c637ab3d387c44b
Zozano <private@private.com>
2025-04-10 17:36:18 +1000

    2025-04-10_17:36:18

--- z-nixos/modules/xmrig.nix
+++ z-nixos/modules/xmrig.nix
@@ -1,43 +1,23 @@
 { config, pkgs, ... }:
 
-let
-  xmrigConfigJSON = builtins.toJSON {
-    pools = [
-      {
-        url = "pool.supportxmr.com:9000";
-        user = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";
-        pass = "x"; # optional password
-        tls = true;
-        keepalive = true;
-      }
-    ];
-    cpu = { enabled = true; };
-    opencl = { enabled = false; };
-    cuda = { enabled = false; };
-    donate-level = 1;
-  };
-in
 {
-  config = {
-    environment.systemPackages = [ pkgs.xmrig ];
+  services.xmrig = {
+    enable = true;
+    settings = {
 
-    # Write the XMRig config to /etc
-    environment.etc."xmrig/config.json".text = xmrigConfigJSON;
-
-    # Enable huge pages (recommended for RandomX)
-    boot.kernel.sysctl."vm.nr_hugepages" = 128;
-
-    systemd.services.xmrig = {
-      description = "XMRig Miner";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.xmrig}/bin/xmrig --config=/etc/xmrig/config.json";
-        Restart = "always";
-        Nice = 10;
-        NoNewPrivileges = true;
-      };
+      autosave = true;
+      cpu = true;
+      opencl = false;
+      cuda = false;
+     
+      pools = [
+        {
+          url = "pool.supportxmr.com:443";
+          user = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";
+          keepalive = true;
+          tls = true;
+        };
+      ];
     };
   };
 }
-
