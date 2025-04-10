96b62190d95a857fca417ed74b5fd8e87de221fe
Zozano <private@private.com>
2025-04-10 17:40:10 +1000

    2025-04-10_17:40:10

--- z-nixos/modules/xmrig.nix
+++ z-nixos/modules/xmrig.nix
@@ -12,7 +12,7 @@
      
       pools = [
         {
-          url = "pool.supportxmr.com:443";
+          url = "pool-aus.supportxmr.com:443";
           user = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";
           keepalive = true;
           tls = true;
