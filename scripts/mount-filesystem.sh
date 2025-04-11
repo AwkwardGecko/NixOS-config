601f5d69843c3c686eb21db6109c5212620b3c17
Zozano <private@private.com>
2025-04-11 12:44:13 +1000

    2025-04-11_12:44:13

---
+++ scripts/mount-filesystem.sh
@@ -0,0 +1,1 @@
+sudo sshfs z-home@192.168.1.157:/data /server
--- z-nixos/modules/xmrig.nix
+++ z-nixos/modules/xmrig.nix
@@ -10,12 +10,15 @@
       cpu = true;
       opencl = false;
       cuda = false;
+
+      donate-level = 0;
       
       # CPU settings
       cpu-priority = 5;
-      cpu-usage = 75;
-      threads = 6;
-
+      #cpu-usage = 75;
+      cpu-usage = null;
+      # threads = 6;
+      threads = null;
 
       # RandomX optimizations
       randomx = {
@@ -30,7 +33,8 @@
           url = "xmr-eu2.nanopool.org:10343";
           # url = "pool.supportxmr.com:8080";
           user = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";
-          algo = "rx/0";
+          #algo = "rx/0";
+          algo = "rx";
           keepalive = true;
           tls = true;
         }
