56cfe23b30a915669d2c2c86ae3dd0f3d3ba4132
Zozano <private@private.com>
2025-04-12 13:42:19 +1000

    2025-04-12_13:42:19

--- z-nixos/modules/monero.nix
+++ z-nixos/modules/monero.nix
@@ -6,14 +6,20 @@
     enable = true;
     dataDir = "/steam/Monero";
     
-    extraFlags = [
-      "--out-peers" "64"
-      "--prune-blockchain"
-      "--enable-dns-blocklist"
-      "--max-concurrency" "8"
-      "--block-sync-size" "20"
-      "--db-sync-mode" "fast:async:1000"
-    ];
+  systemd.services.monero = {
+    serviceConfig = {
+      ExecStart = lib.mkForce ''
+        ${pkgs.monero}/bin/monerod \
+          --data-dir /steam/Monero \
+          --out-peers 64 \
+          --prune-blockchain \
+          --enable-dns-blocklist \
+          --max-concurrency 8 \
+          --block-sync-size 20 \
+          --db-sync-mode fast:async:1000
+      '';
+    };
+  };
 
     #mining.enable = true;
     #mining.address = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";  # Replace with your Monero wallet address
