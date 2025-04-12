b2aab473241911bb3c1c7e18226c716bb778e0f2
Zozano <private@private.com>
2025-04-12 13:28:26 +1000

    2025-04-12_13:28:26

--- z-nixos/modules/bluetooth.nix
+++ z-nixos/modules/bluetooth.nix
@@ -14,20 +14,20 @@
   boot.blacklistedKernelModules = [ "xpad" ];
   
 
-  #boot.kernelPackages = pkgs.linuxPackages_zen_6_12;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
   boot.initrd.kernelModules = [ 
     "joydev"
     "uhid"
-    "hid_xpadneo"
+    #"hid_xpadneo"
   ];
 
-  #environment.systemPackages = with pkgs; [
-    #linuxKernel.packages.linux_zen.xpadneo
+  environment.systemPackages = with pkgs; [
+    linuxKernel.packages.linux_zen.xpadneo
     #bluez-experimental
     #bluez-alsa
     #bluez-tools
-  #];
+  ];
 
   hardware.bluetooth = {
     enable = true;
