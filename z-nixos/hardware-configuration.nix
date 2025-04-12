907cd5c329055812500f540a0d4307a24e9e962c
Zozano <private@private.com>
2025-04-12 10:31:31 +1000

    2025-04-12_10:31:31

--- z-nixos/hardware-configuration.nix
+++ z-nixos/hardware-configuration.nix
@@ -32,6 +32,11 @@
   fileSystems."/steam" =
     { device = "/dev/disk/by-uuid/249c8bec-3ec2-4b89-8618-748cd918d4ba";
       fsType = "btrfs";
+      options = [
+        "compress=zstd"
+        "space_cache=v2"
+
+      ]
     };
 
   fileSystems."/server" =
