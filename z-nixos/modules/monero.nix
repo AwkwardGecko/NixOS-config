dc4653ebc37b4b14cfb264fa81e491c27383ff83
Zozano <private@private.com>
2025-04-12 14:06:10 +1000

    2025-04-12_14:06:10

--- z-nixos/modules/monero.nix
+++ z-nixos/modules/monero.nix
@@ -1,17 +1,17 @@
 { lib, pkgs, config, ... }:
 
 {
-  # Enable Monero service (for node management)
-  services.monero = {
-    enable = true;
-    dataDir = "/steam/Monero";
-    
-
-
-    #mining.enable = true;
-    #mining.address = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";  # Replace with your Monero wallet address
-    #mining.threads = 6;  # Adjust based on how many threads you want to allocate for mining
-  };
+  # # Enable Monero service (for node management)
+  # services.monero = {
+  #   enable = true;
+  #   dataDir = "/steam/Monero";
+  #   
+  #
+  #
+  #   #mining.enable = true;
+  #   #mining.address = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";  # Replace with your Monero wallet address
+  #   #mining.threads = 6;  # Adjust based on how many threads you want to allocate for mining
+  # };
 
   environment.systemPackages = with pkgs; [
     monero-gui
