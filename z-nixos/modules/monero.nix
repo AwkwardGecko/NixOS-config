568b1bdc58c00905af0f636efcca140184c0d3cf
Zozano <private@private.com>
2025-04-12 13:43:41 +1000

    2025-04-12_13:43:41

--- z-nixos/modules/monero.nix
+++ z-nixos/modules/monero.nix
@@ -6,6 +6,13 @@
     enable = true;
     dataDir = "/steam/Monero";
     
+
+
+    #mining.enable = true;
+    #mining.address = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";  # Replace with your Monero wallet address
+    #mining.threads = 6;  # Adjust based on how many threads you want to allocate for mining
+  };
+
   systemd.services.monero.serviceConfig.ExecStart = lib.mkForce ''
     ${pkgs.monero}/bin/monerod \
       --data-dir /steam/Monero \
@@ -17,9 +24,5 @@
       --db-sync-mode fast:async:1000
   '';
 
-    #mining.enable = true;
-    #mining.address = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";  # Replace with your Monero wallet address
-    #mining.threads = 6;  # Adjust based on how many threads you want to allocate for mining
-  };
 
 }
