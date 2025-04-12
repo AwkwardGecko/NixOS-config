6a801e5de0b19f9a8c868be693d8d57aa9e7ef4b
Zozano <private@private.com>
2025-04-12 13:13:27 +1000

    2025-04-12_13:13:27

--- z-nixos/modules/bluetooth.nix
+++ z-nixos/modules/bluetooth.nix
@@ -2,71 +2,42 @@
 ### BLUETOOTH ###
 #################
 
-# {
-#   config,
-#   pkgs,
-#   lib,
-#   ...
-# }:
-# {
-#
-#   #boot.kernelPackages = pkgs.linuxPackages_zen_6_12;
-#   boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
-#   boot.initrd.kernelModules = [ 
-#     "joydev"
-#     "uhid"
-#     "hid_xpadneo"
-#   ];
-#   
-#   #environment.systemPackages = with pkgs; [
-#     #linuxKernel.packages.linux_zen.xpadneo
-#     #bluez-experimental
-#     #bluez-alsa
-#     #bluez-tools
-#   #];
-#
-#   hardware.bluetooth = {
-#     enable = true;
-#     powerOnBoot = true;
-#     package = pkgs.bluez;
-#     settings.General = {
-#       Privacy = "device";
-#       JustWorksRepairing = "always";
-#       FastConnectable = "true";
-#     };
-#   };
-#
-#   services.blueman.enable = true;
-#
-#   environment.variables = {
-#     "BLUETOOTH_ENABLE_EXPERIMENTAL" = "1";
-#   };
-#
-#
-#}
-
-
-
-{ config, pkgs, lib, ... }:
-
-{ config, pkgs, lib, ... }:
+{
+   config,
+   pkgs,
+   lib,
+   ...
+}:
 
 {
-  # Enable Bluetooth support
-  networking.bluetooth.enable = true;
 
-  # Package for BlueZ Bluetooth stack
-  networking.bluetooth.package = pkgs.bluez;
+  #boot.kernelPackages = pkgs.linuxPackages_zen_6_12;
+  boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
+  boot.initrd.kernelModules = [ 
+    "joydev"
+    "uhid"
+    #"hid_xpadneo"
+  ];
 
-  # Optional: Disable Enhanced Re-Transmission Mode (ERTM) for compatibility
-  networking.bluetooth.extraModprobeConfig = ''
-    options bluetooth disable_ertm=1
-  '';
+  environment.systemPackages = with pkgs; [
+    linuxKernel.packages.linux_zen.xpadneo
+    #bluez-experimental
+    #bluez-alsa
+    #bluez-tools
+  ];
 
-  # Enable Bluetooth on boot and configure the Bluetooth service
-  services.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    General.Experimental = true;
+    settings.General = {
+      Privacy = "device";
+      JustWorksRepairing = "always";
+      FastConnectable = "true";
+    };
+  };
 
-  # Bluetooth hardware settings
-  hardware.bluetooth.enable = true;
+  hardware.enableAllFirmware = true;
+
+  services.blueman.enable = true;
 }
-
