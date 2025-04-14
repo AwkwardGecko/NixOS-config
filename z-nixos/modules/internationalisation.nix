6ea31c40837a73d241505266dff455f309a6e734
Zozano <private@private.com>
2025-04-14 11:02:23 +1000

    2025-04-14_11:02:23

--- z-nixos/modules/internationalisation.nix
+++ z-nixos/modules/internationalisation.nix
@@ -10,7 +10,7 @@
 }:
 {
 
-  systemd.timedatectl.timezoone = "Australia/Sydney";
+  systemd.timedatectl.timezone = "Australia/Sydney";
   time.timeZone = "Australia/Sydney";
 
   i18n.defaultLocale = "en_AU.UTF-8";
