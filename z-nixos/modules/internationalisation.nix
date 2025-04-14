dafdfd6ebfbe6bcbdb43514d44e2afd4e49cc202
Zozano <private@private.com>
2025-04-14 12:05:38 +1000

    2025-04-14_12:05:38

--- z-nixos/modules/internationalisation.nix
+++ z-nixos/modules/internationalisation.nix
@@ -14,8 +14,15 @@
 
   i18n.defaultLocale = "en_AU.UTF-8";
 
+  supportedLocales = [
+    "en_AU.UTF-8"
+    "en_US.UTF-8"
+    "en_DK.UTF-8"
+  ]
+
   i18n.extraLocaleSettings = {
 
+    LANGUAGE = "en_AU.UTF-8";
     LC_ADDRESS = "en_AU.UTF-8";
     LC_IDENTIFICATION = "en_AU.UTF-8";
     LC_MEASUREMENT = "en_AU.UTF-8";
@@ -24,6 +31,6 @@
     LC_NUMERIC = "en_AU.UTF-8";
     LC_PAPER = "en_AU.UTF-8";
     LC_TELEPHONE = "en_AU.UTF-8";
-    LC_TIME = "en_AU.UTF-8";
+    LC_TIME = "en_DK.UTF-8";
   };
 }
