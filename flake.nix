d5a0a7189957c83833cc478eb17b7c0893ff0cee
Zozano <private@private.com>
2025-04-15 10:54:01 +1000

    2025-04-15_10:54:01

--- flake.nix
+++ flake.nix
@@ -1,12 +1,7 @@
 {
-  # to create the flake.lock file with user permissions, run:
-  # $ nix flake lock
-
   description = "Flake File";
 
-
   inputs = {
-
     nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
@@ -24,51 +19,55 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nix-comfyui = {
+      url = "github:dyscorv/nix-comfyui";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    
-    inputs@{
-      self,
-      nixpkgs,
-      home-manager,
-      aagl,
-      nixvim,
-      ...
-    }:
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    home-manager,
+    aagl,
+    nixvim,
+    nix-comfyui,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    nixosConfigurations = {
+      z-nixos = nixpkgs.lib.nixosSystem {
+        system = system;
+        modules = [
+          ./z-nixos/configuration.nix
+          home-manager.nixosModules.home-manager
+          nixvim.nixosModules.nixvim
+          {
+            imports = [ aagl.nixosModules.default ];
+            nix.settings = aagl.nixConfig;
+            programs.honkers-railway-launcher.enable = true;
 
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in
-
-    {
-      nixosConfigurations = {
-        z-nixos = nixpkgs.lib.nixosSystem {
-          #system = "x86_64-linux";
-          system = system;
-          modules = [
-            ./z-nixos/configuration.nix
-            home-manager.nixosModules.home-manager
-            nixvim.nixosModules.nixvim
-            { 
-              imports = [ aagl.nixosModules.default ];
-              nix.settings = aagl.nixConfig;
-              programs.honkers-railway-launcher.enable = true;
-
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.zozano = import ./home-manager/home.nix;
-            }
-          ]; #modules end
-         
-          specialArgs = {
-            inherit inputs;
-          };
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.zozano = import ./home-manager/home.nix;
+          }
+        ];
+        specialArgs = {
+          inherit inputs;
         };
       };
     };
-  } 
+
+    # Expose comfyui as a runnable app with `nix run`
+    apps.${system}.default = nix-comfyui.apps.${system}.default;
+
+    # If you want to pull in packages (e.g. to include in your system)
+    packages.${system}.comfyui = nix-comfyui.packages.${system}.default;
+  };
+}
+
