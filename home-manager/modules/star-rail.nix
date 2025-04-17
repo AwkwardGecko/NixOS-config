7dd71d22f6f785d59554fc8dda6d8c40d6b32634
Zozano <private@private.com>
2025-04-18 09:29:22 +1000

    2025-04-18_09:29:22

--- home-manager/modules/star-rail.nix
+++
@@ -1,8 +0,0 @@
-let
-  aagl-gtk-on-nix = import (
-    builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz"
-  );
-in
-{
-  home.packages = [ aagl-gtk-on-nix.the-honkers-railway-launcher ];
-}
