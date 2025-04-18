510b666e28aea89e238e88b621fe5a4f3e924246
Zozano <private@private.com>
2025-04-18 10:03:53 +1000

    2025-04-18_10:03:53

--- z-nixos/modules/chromium.nix
+++ z-nixos/modules/chromium.nix
@@ -3,6 +3,7 @@
 {
   programs.chromium = {
     enable = true;
+    package = pkgs.chromium;
     extraOpts = {
       "homepage" = "http://localhost:8188";
       "session.restore_on_startup" = 4; # Open specific set of URLs
