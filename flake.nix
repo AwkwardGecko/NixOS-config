32229953098dd9d842f19100a5da08e7f396f0c0
Zozano <private@private.com>
2025-04-19 19:28:38 +1000

    2025-04-19_19:28:38

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
           pkgs.comfyuiPackages.extensions.acly-tooling
           pkgs.comfyuiPackages.extensions.cubiq-ipadapter-plus
           pkgs.comfyuiPackages.extensions.fannovel16-controlnet-aux
-          pkgs.comfyuiPackages.extensions.ComfyUI-Manager
+          comfyui-manager
         ];
 
         commandLineArgs = [
