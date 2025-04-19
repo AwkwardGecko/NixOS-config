23b38c767218c22379dfbfbb4b998ea873ebe020
Zozano <private@private.com>
2025-04-19 11:49:47 +1000

    2025-04-19_11:49:47

--- z-nixos/configuration.nix
+++ z-nixos/configuration.nix
@@ -150,7 +150,7 @@
     dbus                        # IPC system used by desktop apps
     dcap                        # Data Center Attestation Primitives (SGX stuff)
     dig                         # DNS Lookup
-    digikam                     # Photo management software
+    #digikam                     # Photo management software
     docker                      # Container engine
     docker-compose              # Define & run multi-container apps with Docker
     evince                      # Document viewer
