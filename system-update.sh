e0f6959e804d6805c33d38f3790833b2f094da40
Zozano <private@private.com>
2025-04-20 13:42:42 +1000

    updated system-update.sh

--- system-update.sh
+++ system-update.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 cd /home/zozano/.dotfiles
-git add *
-git commit -m "$(date '+%F_%H:%M:%S')"
-git push github main
-
 nix flake update
 sudo nixos-rebuild switch --upgrade --flake /home/zozano/.dotfiles/#z-nixos
-
