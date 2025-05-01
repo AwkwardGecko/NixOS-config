19d92b44522f693104fa62ede5a30dc259b2d8dc
Zozano <private@private.com>
2025-05-01 10:52:31 +1000

    2025-05-01_10:52:31

--- home-manager/modules/xmrig.nix
+++ home-manager/modules/xmrig.nix
@@ -3,9 +3,9 @@
 {
 
   systemd.user.services.xmrig = {
-    Install = {
-      WantedBy = [ "default.target" ]; 
-    };
+    # Install = {
+    #   WantedBy = [ "default.target" ]; 
+    # };
     Unit = {
       Description = "xmrig miner (user service)";
       After = [ "default.target" ];
