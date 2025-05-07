c668d357cee85693d80a9a06812afb12201f0142
Zozano <private@private.com>
2025-05-08 01:08:35 +1000

    2025-05-08_01:08:35

--- z-nixos/modules/hdd-soft-shutdown.nix
+++ z-nixos/modules/hdd-soft-shutdown.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
+
 {
-
-systemd.services.hdd-powerdown = {
-  description = "Spin down HDD before shutdown";
-  wantedBy = [ "shutdown.target" ];  # This ensures the service runs during shutdown
-  execStart = "/usr/bin/hdparm -y /dev/sdc";  # Replace /dev/sdc with your actual drive path
-  restart = "no";  # Don't try to restart this service
-};
-
+  systemd.services.hdd-powerdown = {
+    description = "Spin down HDD before shutdown";
+    wantedBy = [ "shutdown.target" ];
+    serviceConfig.ExecStart = "${pkgs.hdparm}/bin/hdparm -y /dev/sdc";  # Ensure this points to the correct drive
+    restart = "no";
+  };
 }
+
