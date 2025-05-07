dfabe86cf304d0c1523ce2093319ec18315861bb
Zozano <private@private.com>
2025-05-07 19:58:28 +1000

    2025-05-07_19:58:28

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -172,7 +172,7 @@
           icon-size = 20;
           icon-spacing = 0;
           format = " GPU {output}°C";
-          exec = "/run/current-system/sw/bin/nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits";
+          exec =  ''sh -c "/run/current-system/sw/bin/nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits"'';
           interval = 10;
          };
 
