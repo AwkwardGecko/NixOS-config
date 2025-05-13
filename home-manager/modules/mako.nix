e85fe0eb6a986372771eae6050e663ea460a5714
Zozano <private@private.com>
2025-05-13 12:14:25 +1000

    2025-05-13_12:14:25

--- home-manager/modules/mako.nix
+++ home-manager/modules/mako.nix
@@ -16,11 +16,8 @@
     defaultTimeout = "2";
     icons = true;
     font = "monospace";
-    backgroundColor = "#${config.colorScheme.palette.base01}";
-    #borderColor = "#${config.colorScheme.palette.base0E}";
     borderRadius = "15";
     borderSize = "2";
-    textColor = "#${config.colorScheme.palette.base04}";
     layer = "overlay";
   };
 }
