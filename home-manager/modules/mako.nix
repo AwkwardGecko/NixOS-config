4972dfce79d5306a72f82cc2ccb9a2bfeff49d2d
Zozano <private@private.com>
2025-05-13 12:14:10 +1000

    2025-05-13_12:14:10

--- home-manager/modules/mako.nix
+++ home-manager/modules/mako.nix
@@ -17,7 +17,7 @@
     icons = true;
     font = "monospace";
     backgroundColor = "#${config.colorScheme.palette.base01}";
-    borderColor = "#${config.colorScheme.palette.base0E}";
+    #borderColor = "#${config.colorScheme.palette.base0E}";
     borderRadius = "15";
     borderSize = "2";
     textColor = "#${config.colorScheme.palette.base04}";
