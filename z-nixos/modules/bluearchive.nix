3961aa2fc2b19ca4433e72fa6002d8654104620c
Zozano <private@private.com>
2025-05-13 12:41:18 +1000

    2025-05-13_12:41:18

--- z-nixos/modules/bluearchive.nix
+++ z-nixos/modules/bluearchive.nix
@@ -2,4 +2,17 @@
 
 {
   virtualisation.waydroid.enable = true;
+
+    boot.extraModulePackages = with config.boot.kernelPackages; [
+    waydroidModules
+  ];
+
+  boot.kernelModules = [
+    "binder_linux"
+    "ashmem_linux"
+  ];
+
+  boot.extraModprobeConfig = ''
+    options binder_linux devices=binder,hwbinder,vndbinder
+  '';
 }
