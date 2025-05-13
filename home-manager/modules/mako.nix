926d0803041539bcd135af81f312467a19ffd983
Zozano <private@private.com>
2025-05-13 12:22:16 +1000

    2025-05-13_12:22:16

--- home-manager/modules/mako.nix
+++ home-manager/modules/mako.nix
@@ -13,11 +13,13 @@
 
   services.mako = {
     enable = true; # notification daemon
-    defaultTimeout = "2";
-    icons = true;
-    font = "monospace";
-    borderRadius = "15";
-    borderSize = "2";
-    layer = "overlay";
+    settings = {
+      defaultTimeout = "2";
+      icons = true;
+      font = "JetBrainsMono-Regular";
+      borderRadius = "15";
+      borderSize = "2";
+      layer = "overlay";
+    };
   };
 }
