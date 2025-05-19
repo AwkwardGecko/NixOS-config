18826644591bc794f654975ceb9a5109b31974b3
Zozano <private@private.com>
2025-05-19 13:36:16 +1000

    2025-05-19_13:36:16

--- scripts/delete-old-files.sh
+++ scripts/delete-old-files.sh
@@ -1,3 +1,8 @@
 #~/usr/bin/env bash
 
 sudo nix-collect-garbage -d --delete-older-than 7d
+sudo nix store gc
+nix profile wipe-history --older-than 7d
+sudo rm -rf ~/.cache/nix
+sudo find /nix/var/nix/gcroots/auto -xtype l -delete
+sudo nix-store --optimise
