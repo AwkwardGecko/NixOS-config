031cbf07e0be8b0ea56b914d78d7d26470a702d3
Zozano <private@private.com>
2025-05-28 14:00:51 +1000

    2025-05-28_14:00:51

--- home-manager/modules/mako.nix
+++ home-manager/modules/mako.nix
@@ -16,7 +16,7 @@
     settings = {
       actions = true;
       anchor = "top-right";
-      background-color = "#000000";
+      background-color = "#000000CC";
       border-color = "#FFFFFF";
       border-radius = 0;
       default-timeout = 0;
@@ -29,6 +29,19 @@
       markup = true;
       defaultTimeout = "2";
       font = "JetBrainsMono 10";
+
+      urgency-low = {
+        border-color = "#444444";
+      };
+
+      urgency-normal = {
+        border-color = "#888888";
+      };
+
+      urgency-critical = {
+        border-color = "#FF0000";
+        default-timeout = 0; # stay until dismissed
+      };
     };
   };
 }
