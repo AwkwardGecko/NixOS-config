3a51531dd8741b6b69658424c17666f7998e2482
Zozano <private@private.com>
2025-06-10 15:33:55 +1000

    2025-06-10_15:33:55

--- home-manager/modules/hypr.nix
+++ home-manager/modules/hypr.nix
@@ -45,7 +45,7 @@
       #"sleep 10 && bash ~/.local/share/applications/mount-server.sh"
       #"sleep 15 && bash ~/.local/share/applications/mount-music.sh"
       "hypridle"
-      "mako"
+      "sleep 5 && mako"
     ];
 
     env = [
