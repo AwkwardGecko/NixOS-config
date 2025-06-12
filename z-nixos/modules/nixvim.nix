2b241ea0c76942cc4044928a96f1ae177d39b7f2
Zozano <private@private.com>
2025-06-12 19:11:07 +1000

    2025-06-12_19:11:07

--- z-nixos/modules/nixvim.nix
+++ z-nixos/modules/nixvim.nix
@@ -9,24 +9,24 @@
     colorschemes.gruvbox.enable = true;  # Gruvbox colorscheme
 
 
-    # extraConfigLua = ''
-    #   local format_enabled = true
-    #   vim.api.nvim_create_user_command(
-    #     "ToggleFormatNotified",
-    #     function()
-    #     if format_enabled then
-    #       vim.cmd("FormatDisable")
-    #       require("notify")("Disabled formatting")
-    #       format_enabled = false
-    #     else
-    #       vim.cmd("FormatEnable")
-    #       require("notify")("Enabled formatting")
-    #       format_enabled = true
-    #       end
-    #     end,
-    #     {}
-    #   )
-    # '';
+    extraConfigLua = ''
+      local format_enabled = true
+      vim.api.nvim_create_user_command(
+        "ToggleFormatNotified",
+        function()
+        if format_enabled then
+          vim.cmd("FormatDisable")
+          require("notify")("Disabled formatting")
+          format_enabled = false
+        else
+          vim.cmd("FormatEnable")
+          require("notify")("Enabled formatting")
+          format_enabled = true
+          end
+        end,
+        {}
+      )
+    '';
 
 
     plugins = {
