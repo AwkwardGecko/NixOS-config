3b56682c2f9fdd12f0a66fb75bcca4ac6fefb38b
Zozano <private@private.com>
2025-06-15 13:21:42 +1000

    2025-06-15_13:21:42

--- home-manager/modules/diffusion.nix
+++ home-manager/modules/diffusion.nix
@@ -4,7 +4,7 @@
   xdg.desktopEntries.diffusion = {
     name = "Diffusion";
     comment = "Mount encrypted drive and launch ComfyUI";
-    exec = "${pkgs.kitty}/bin/kitty --hold sh -c \"sudo cryptsetup open /dev/disk/by-uuid/c5e87ce4-523f-46b9-8735-c6e7545a6d56 luks && sudo mount /dev/mapper/luks /mnt/luks\"";
+    exec = "${pkgs.kitty}/bin/kitty --hold sh -c \"sudo cryptsetup open /dev/disk/by-uuid/c5e87ce4-523f-46b9-8735-c6e7545a6d56 luks && sudo mount /dev/mapper/luks /mnt/luks\" && kitty bash -c \"cd ~/test-shell && NIXPKGS_ALLOW_UNFREE=1 nix-shell --run \"cd ComfyUI && python main.py --lowvram\"\"";
     icon = "/home/zozano/.local/share/icons/comfyui.png";
     terminal = false;
     type = "Application";
