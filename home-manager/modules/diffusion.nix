eb8d01250a5b74c44800deac335874ead940a2cb
Zozano <private@private.com>
2025-06-15 13:16:17 +1000

    2025-06-15_13:16:17

--- home-manager/modules/diffusion.nix
+++ home-manager/modules/diffusion.nix
@@ -3,9 +3,9 @@
 {
   xdg.desktopEntries.diffusion = {
     name = "Diffusion";
-    comment = "Mount encrypted drive and launch ComfyUI"
+    comment = "Mount encrypted drive and launch ComfyUI";
     exec = ''
-        ${pkgs.kitty}/bin/kitty --hold sh -c '
+      ${pkgs.kitty}/bin/kitty --hold sh -c '
         sudo cryptsetup open /dev/disk/by-uuid/c5e87ce4-523f-46b9-8735-c6e7545a6d56 luks &&
         sudo mount /dev/mapper/luks /mnt/luks &&
       '
