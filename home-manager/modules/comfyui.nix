df85d33851a0dd9ae0428cbf82ddb778347e7100
Zozano <private@private.com>
2025-06-15 13:05:19 +1000

    2025-06-15_13:05:19

--- home-manager/modules/comfyui.nix
+++ home-manager/modules/comfyui.nix
@@ -3,10 +3,11 @@
 {
   xdg.desktopEntries.comfyui = {
     name = "ComfyUI";
-    exec = "kitty bash -c 'cd ~/test-shell && NIXPKGS_ALLOW_UNFREE=1 nix-shell --run \"cd ComfyUI && python main.py --lowvram\"'";
-    icon = "${config.home.homeDirectory}/.local/share/icons/comfyui.png";
+    exec = "kitty bash -c \"cd /home/zozano/test-shell && NIXPKGS_ALLOW_UNFREE=1 nix-shell --run 'cd ComfyUI && python main.py --lowvram'\"";
+    icon = "/home/zozano/.local/share/icons/comfyui.png";
     terminal = false;
     type = "Application";
-    categories = [ "Media" ];
+    categories = [ "Graphics" ];
   };
 }
+
