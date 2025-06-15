e777c63816a755348ff94510a04531e6aef23114
Zozano <private@private.com>
2025-06-15 13:16:54 +1000

    2025-06-15_13:16:54

--- home-manager/modules/diffusion.nix
+++ home-manager/modules/diffusion.nix
@@ -7,10 +7,9 @@
     exec = ''
       ${pkgs.kitty}/bin/kitty --hold sh -c '
         sudo cryptsetup open /dev/disk/by-uuid/c5e87ce4-523f-46b9-8735-c6e7545a6d56 luks &&
-        sudo mount /dev/mapper/luks /mnt/luks &&
-      '
+        sudo mount /dev/mapper/luks /mnt/luks'
     '';
-    #icon = "/home/zozano/.local/share/icons/comfyui.png";
+    icon = "/home/zozano/.local/share/icons/comfyui.png";
     terminal = true;
     type = "Application";
     categories = [ "Graphics" ];
