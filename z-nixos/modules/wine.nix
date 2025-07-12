425b8440410de929073d725436852778fd690f1b
Zozano <private@private.com>
2025-07-12 20:21:54 +1000

    2025-07-12_20:21:54

--- z-nixos/modules/wine.nix
+++ z-nixos/modules/wine.nix
@@ -5,10 +5,10 @@
   environment.systemPackages = with pkgs; [
     
     # support both 32-bit and 64-bit applications
-    wineWowPackages.stable
+    #wineWowPackages.stable
 
     # wine-staging (version with experimental features)
-    wineWowPackages.staging
+    #wineWowPackages.staging
 
     # winetricks (all versions)
     winetricks
