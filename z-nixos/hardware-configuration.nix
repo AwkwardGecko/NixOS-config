5d0cd0366a4b1f6512eb465a6a8e2ab1608327d0
Zozano <private@private.com>
2025-07-15 12:44:44 +1000

    2025-07-15_12:44:44

--- z-nixos/hardware-configuration.nix
+++ z-nixos/hardware-configuration.nix
@@ -52,6 +52,17 @@
       ];
     };
 
+    "/home/zozano/.var/app/moe.launcher.honkers-launcher" = {
+      device = "/dev/disk/by-uuid/249c8bec-3ec2-4b89-8618-748cd918d4ba";
+      fsType = "btrfs";
+      options = [
+        "subvol=@honkai-impact"
+        "space_cache=v2"
+        "discard=async"
+        "nofail"
+      ];
+    };
+
     "/home/zozano/.var/app/moe.launcher.the-honkers-railway-launcher" = {
       device = "/dev/disk/by-uuid/249c8bec-3ec2-4b89-8618-748cd918d4ba";
       fsType = "btrfs";
