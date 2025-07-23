16394a7d85e7b0cb6da9ea4badf899ce2d1b9467
Zozano <private@private.com>
2025-07-23 16:41:11 +1000

    2025-07-23_16:41:11

--- home-manager/modules/comfyui.nix
+++ home-manager/modules/comfyui.nix
@@ -8,8 +8,8 @@ let
 
     # GPU mode
     sudo nvidia-smi -i 0 -c EXCLUSIVE_PROCESS
-   
-    export __GL_FRAMEBUFFER_SRGB_CAPABLE=1
+  
+    export WINIT_UNIX_NO_PORTAL=1 comfyuistart
     export NIXPKGS_ALLOW_UNFREE=1
     export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
     export COMFYUI_PORT=8188
