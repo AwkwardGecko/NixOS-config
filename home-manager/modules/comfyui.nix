59eb14a6ed22a48fda063a2512a6b124e28c6b4b
Zozano <private@private.com>
2025-07-23 15:21:32 +1000

    2025-07-23_15:21:32

--- home-manager/modules/comfyui.nix
+++ home-manager/modules/comfyui.nix
@@ -1,12 +1,37 @@
 #~/.dotfiles/home-manager/modules/comfyui.nix
 { config, lib, pkgs, ... }:
+
+let
+  comfyScript = ''
+    #!/usr/bin/env bash
+    sudo nvidia-smi -i 0 -c EXCLUSIVE_PROCESS
+    export NIXPKGS_ALLOW_UNFREE=1
+    export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
+    cd ~/test-shell/ComfyUI
+    git pull origin master
+    /home/zozano/test-shell/.venv/bin/python -s -m pip install -r /home/zozano/test-shell/Comfyui/requirements.txt
+    python main.py \
+      --lowvram \
+      --force-fp16 \
+      --use-split-cross-attention \
+      --preview-method auto \
+      --reserve-vram 0.5 \
+      --disable-smart-memory
+  '';
+in
 {
   xdg.desktopEntries.comfyui = {
     name = "ComfyUI";
-    exec = "kitty bash -c \"cd /home/zozano/test-shell && NIXPKGS_ALLOW_UNFREE=1 nix-shell --run 'cd ComfyUI && python main.py --lowvram'\"";
+    exec = "kitty bash -c run-comfy.sh";
+    # exec = "kitty bash -c \"cd /home/zozano/test-shell && NIXPKGS_ALLOW_UNFREE=1 nix-shell --run 'cd ComfyUI && python main.py --lowvram'\"";
     icon = "/home/zozano/.local/share/icons/comfyui.png";
-    terminal = false;
+    terminal = true;
     type = "Application";
     categories = [ "Graphics" ];
+  };
+
+  home.file.".local/bin/run-comfy.sh" = {
+    executable = true;
+    text = comfyScript;
   };
 }
