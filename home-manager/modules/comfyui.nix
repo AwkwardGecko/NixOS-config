e2e0781ea543e31600e5dfd3cf1a92eb98770793
Zozano <private@private.com>
2025-07-23 15:27:22 +1000

    2025-07-23_15:27:22

--- home-manager/modules/comfyui.nix
+++ home-manager/modules/comfyui.nix
@@ -9,15 +9,16 @@ let
     export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
     cd ~/test-shell/ComfyUI
     git pull origin master
-    /home/zozano/test-shell/.venv/bin/python -m pip install --upgrade pip
-    /home/zozano/test-shell/.venv/bin/python -s -m pip install -r /home/zozano/test-shell/ComfyUI/requirements.txt
+    cd ~/test-shell && NIXPKGS_ALLOW_UNFREE=1 nix-shell --run \
+    '/home/zozano/test-shell/.venv/bin/python -m pip install --upgrade pip \
+    /home/zozano/test-shell/.venv/bin/python -s -m pip install -r /home/zozano/test-shell/ComfyUI/requirements.txt \
     python main.py \
       --lowvram \
       --force-fp16 \
       --use-split-cross-attention \
       --preview-method auto \
       --reserve-vram 0.5 \
-      --disable-smart-memory
+      --disable-smart-memory'
   '';
 in
 {
