60cc34b9e637d703e3f5b8dec83438f50b11bb50
Zozano <private@private.com>
2025-07-25 10:22:23 +1000

    2025-07-25_10:22:23

--- z-nixos/modules/nixvim.nix
+++ z-nixos/modules/nixvim.nix
@@ -88,7 +88,7 @@
         autoEnableSources = true;
       };
      
-      cmp.buffer.enable = true;
+      cmp-buffer.enable = true;
       cmp-path.enable = true;
       cmp-nvim-lsp.enable = true;
       cmp_luasnip.enable = true;
