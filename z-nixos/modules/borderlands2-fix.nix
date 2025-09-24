e579a1277fff6f664efa6f51cee0e720cd5e4ddd
Zozano <private@private.com>
2025-09-24 15:00:44 +1000

    2025-09-24_15:00:44

--- z-nixos/modules/borderlands2-fix.nix
+++ z-nixos/modules/borderlands2-fix.nix
@@ -3,7 +3,9 @@
   systemd.tmpfiles.rules = [
     "r! /usr/lib/ssl"
     "L+ /usr/lib/ssl - - - - /etc/ssl"
-
     "L+ /usr/lib/ssl/cert.pem - - - - /etc/ssl/certs/ca-bundle.crt"
   ];
+
+  services.resolved.enable = true;
+  environment.etc."resolv.conf".source = "/run/systemd/resolve/resolv.conf";
 }
