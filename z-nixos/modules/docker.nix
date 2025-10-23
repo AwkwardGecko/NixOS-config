b1620b6e8b326e505e5d215e95ccbf15d7973d84
Zozano <private@private.com>
2025-10-23 16:12:41 +1100

    2025-10-23_16:12:41

--- z-nixos/modules/docker.nix
+++ z-nixos/modules/docker.nix
@@ -25,7 +25,6 @@
     nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
     nvidia.modesetting.enable = true;
     nvidia.open = false;
-    nvidiaSettings = true;
     nvidia-container-toolkit.enable = true;
 
     graphics = {
