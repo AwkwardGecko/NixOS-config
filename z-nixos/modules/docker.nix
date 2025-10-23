21ca63a90a632b82ed9e8f6e7976906ff3a51fbb
Zozano <private@private.com>
2025-10-23 14:24:04 +1100

    2025-10-23_14:24:04

--- z-nixos/modules/docker.nix
+++ z-nixos/modules/docker.nix
@@ -15,8 +15,8 @@
 
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
   hardware.nvidia-container-toolkit.enable = true;
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport32Bit = true;
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
 
   users.users.zozano.extraGroups = [ "docker" ];
 }
