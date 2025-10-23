a5524d134e089227771401108230e1bf25decf14
Zozano <private@private.com>
2025-10-23 15:48:18 +1100

    2025-10-23_15:48:18

--- z-nixos/modules/docker.nix
+++ z-nixos/modules/docker.nix
@@ -11,16 +11,25 @@
 {
   virtualisation.docker = {
     enable = true;
+    enableOnBoot = true;
+
     daemon.settings = {
       runtimes.nvidia.path = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
       default-runtime = "nvidia";
     };
   };
 
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
-  hardware.nvidia-container-toolkit.enable = true;
-  hardware.graphics.enable = true;
-  hardware.graphics.enable32Bit = true;
+  hardware = {
+    
+    nvidia.package = config.boot.kernelPackages.nvidiaPackages.production;
+    nvidia-container-toolkit.enable = true;
+
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      setLdLibraryPath = true;
+    };
+  };
 
   users.users.zozano.extraGroups = [ "docker" ];
 }
