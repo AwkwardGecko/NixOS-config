bffffaa517e5714686b1f267ca0fe6af2778d735
Zozano <private@private.com>
2025-10-30 10:34:45 +1100

    2025-10-30_10:34:45

--- z-nixos/modules/controller.nix
+++ z-nixos/modules/controller.nix
@@ -1,19 +1,26 @@
 #~/.dotfiles/z-nixos/modules/controller.nix
 { config, lib, pkgs, ... }:
+let
+  gcdb = pkgs.sdl_gamecontrollerdb;
+  dbPathPreferred = "${gcdb}/share/sdl2/gamecontrollerdb.txt";
+  dbPathAlt       = "${gcdb}/share/gamecontrollerdb/gamecontrollerdb.txt";
+  dbPath = if builtins.pathExists dbPathPreferred then dbPathPreferred else dbPathAlt;
+in
 {
   services.udev.packages = [ pkgs.game-devices-udev-rules ];
 
   hardware = {
-    steam-hardware.enable = true;
+    "steam-hardware".enable = true;
     xone.enable = true;
   };
 
   environment = {
-    systemPackages = with pkgs; [
-      sdl_gamecontrollerdb
+    systemPackages = [
+      gcdb
     ];
+    
     variables = {
-      SDL_GAMECONTROLLERCONFIG = builtins.readFile "${pkgs.sdl_gamecontrollerdb}/share/sdl2/gamecontrollerdb.txt";
+      SDL_GAMECONTROLLERCONFIG = builtins.readFile dbPath;
     };
   };
 }
