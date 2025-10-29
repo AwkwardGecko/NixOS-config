475d0058cfb6f4f2da86e05e2e70bc73a7619a5e
Zozano <private@private.com>
2025-10-30 10:10:37 +1100

    2025-10-30_10:10:37

--- z-nixos/modules/bluetooth.nix
+++ z-nixos/modules/bluetooth.nix
@@ -31,6 +31,7 @@
     options bluetooth disable_ertm=1
     options btusb enable_autosuspend=0
     options hid_xpadneo disable_ff=Y
+    options iwlwifi power_save=0
   '';
 
   #boot.extraModulePackages = [ config.boot.kernelPackages.xpadneo ];
@@ -43,38 +44,5 @@
     wireplumber.enable = true;
   };
 
-
-services.udev.extraRules = ''
-  ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="8087", ATTR{idProduct}=="0029", TEST=="power/control", ATTR{power/control}="on"
-'';
-
-  #services.udev.extraRules = ''
-    # Disable USB autosuspend for Xbox Wireless Controller (045e:0b12)
-  #  ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="0b12", TEST=="power/control", ATTR{power/control}="on"
-    
-    # Also disable USB autosuspend for the Intel Bluetooth adapter (8087:0026)
-  #  ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="8087", ATTR{idProduct}=="0026", TEST=="power/control", ATTR{power/control}="on"
-
-  #'';
-  # --- Xpadneo ---
-
-  # kernelHeaders must match running kernel
-  # boot.extraModulePackages = with config.boot.kernelPackages; [
-  #   xpadneo
-  # ];
-  #
-  # # optional: auto-reload on Bluetooth hot-plug
-  # systemd.services."xpadneo-reload" = {
-  #   after = [ "bluetooth.service" ];
-  #   description = "Reload xpadneo after bluetooth restart";
-  #   wantedBy = [ "bluetooth.service" ];
-  #   serviceConfig.ExecStart = "${pkgs.kmod}/bin/modprobe hid_xpadneo";
-  # };
-
-  # boot.initrd.kernelModules = [ 
-  #   "joydev"
-  #   "uhid"
-  #   "hid_xpadneo"
-  # ];
-
+  services.udev.extraRules = ''ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="8087", ATTR{idProduct}=="0029", TEST=="power/control", ATTR{power/control}="on"'';
 }
--- z-nixos/modules/chromium.nix
+++ z-nixos/modules/chromium.nix
@@ -1,11 +1,22 @@
 { config, lib, pkgs, ... }:
 
 {
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = false; # set true if you want a GUI tray
+
   programs.chromium = {
     enable = true;
+
+    # Add the runtime flag Chromium expects on Linux for Web Bluetooth.
+    commandLineArgs = [
+      "--enable-experimental-web-platform-features"
+    ];
+
+    # Make sure policy isn’t silently blocking the API.
     extraOpts = {
+      "DefaultWebBluetoothGuardSetting" = 3;  # 3 = allow sites to ask
       "homepage" = "http://localhost:8188";
-      "session.restore_on_startup" = 4; # Open specific set of URLs
+      "session.restore_on_startup" = 4;
       "session.startup_urls" = [ "http://localhost:8188" ];
       "browser.show_home_button" = false;
       "profile.default_content_setting_values.notifications" = 2;
@@ -18,8 +29,9 @@
     };
   };
 
-    environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
     chromium
+    bluez
   ];
 }
 
