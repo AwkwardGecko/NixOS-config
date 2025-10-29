7d662332d3d713027fb3bebe5320e369a5a2579c
Zozano <private@private.com>
2025-10-30 10:26:47 +1100

    2025-10-30_10:26:47

--- z-nixos/modules/controller.nix
+++ z-nixos/modules/controller.nix
@@ -1,11 +1,14 @@
 #~/.dotfiles/z-nixos/modules/controller.nix
 { config, lib, pkgs, ... }:
+let
+  sdlDb = pkgs.sdl2-gamecontrollerdb or null;
+in
 {
   hardware.steam-hardware.enable = true;
 
   hardware.xone.enable = true;
 
-  environment.variables = {
-    SDL_GAMECONTROLLERCONFIG = builtins.readFile "${pkgs.sdl2}/share/sdl2/gamecontrollerdb.txt";
+  environment.variables = lib.mkIf (sdlDb != null) {
+    SDL_GAMECONTROLLERCONFIG = builtins.readFile "${pkgs.sdlDb}/share/sdl2/gamecontrollerdb.txt";
   };
 }
