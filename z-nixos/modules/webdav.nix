83cd1294e8491d95490020ee25894f89bb22c5f7
Zozano <private@private.com>
2025-11-04 00:21:43 +1100

    2025-11-04_00:21:43

--- z-nixos/modules/webdav.nix
+++ z-nixos/modules/webdav.nix
@@ -3,12 +3,16 @@
 {
   users.users.webdav = { isSystemUser = true; group = "webdav"; };
   users.groups.webdav = {};
-  systemd.tmpfiles.rules = [ "d /srv/books 0755 webdav webdav -" ];
+
+  systemd.tmpfiles.rules = [
+    "d /srv/books 0755 webdav webdav -"
+    "d /var/lib/httpd 0750 root wwwrun -"
+  ];
 
   services.httpd = {
     enable = true;
     adminAddr = "admin@example.invalid";
-    enableModules = [ "dav" "dav_fs" "auth_basic" ];
+    modules = [ "dav" "dav_fs" "auth_basic" ];
     virtualHosts."_" = {
       listen = [{ ip = "0.0.0.0"; port = 8080; }];
       documentRoot = "/srv/books";
