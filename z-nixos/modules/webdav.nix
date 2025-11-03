ab9f12759dc52fa66428730f0cca3ab0e5f718b1
Zozano <private@private.com>
2025-11-04 00:18:40 +1100

    2025-11-04_00:18:40

--- z-nixos/modules/webdav.nix
+++ z-nixos/modules/webdav.nix
@@ -1,44 +1,51 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   users.users.webdav = {
     isSystemUser = true;
-    createHome = false;
     group = "webdav";
-    #shell = pkgs.nologin;
-    password = "changeme"; # optional if using htpasswd below
   };
-
   users.groups.webdav = {};
 
-  # Directory to serve
+  # Storage
   systemd.tmpfiles.rules = [
     "d /srv/books 0755 webdav webdav -"
   ];
 
-  # Install server binary
-  environment.systemPackages = [ pkgs.webdav-server-rs ];
+  services.nginx = {
+    enable = true;
 
-  # Systemd service
-  systemd.services.webdav = {
-    description = "Simple WebDAV Server";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
+    # Needed for PROPFIND etc.
+    additionalModules = [ pkgs.nginxModules.davExt ];
 
-    serviceConfig = {
-      Restart = "on-failure";
-      ExecStart = "${pkgs.webdav-server-rs}/bin/webdav-server-rs \
-        --host 0.0.0.0 \
-        --port 8080 \
-        --dir /srv/books \
-        --auth-user webdav \
-        --auth-pass changeme";
-      User = "webdav";
-      Group = "webdav";
+    virtualHosts."webdav.local" = {
+      listen = [
+        { addr = "0.0.0.0"; port = 8080; }
+        # add IPv6 if you want:
+        # { addr = "[::]"; port = 8080; }
+      ];
+      root = "/srv/books";
+      basicAuthFile = "/etc/nginx/htpasswd";
+
+      locations."/" = {
+        extraConfig = ''
+          client_max_body_size 2G;
+
+          # WebDAV core
+          dav_methods        PUT DELETE MKCOL COPY MOVE;
+          dav_access         user:rw group:rw all:r;
+          create_full_put_path on;
+
+          # WebDAV extensions
+          dav_ext_methods    PROPFIND OPTIONS;
+          dav_ext_lock_zone  zone=webdav_locks:10m;
+
+          autoindex on;  # lets you see listings in a browser
+        '';
+      };
     };
   };
 
-  # Firewall
   networking.firewall.allowedTCPPorts = [ 8080 ];
 }
 
