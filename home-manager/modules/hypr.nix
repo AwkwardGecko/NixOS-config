8ac7fe59cd003347dc535bb5b8627de3d00a1dd8
Zozano <private@private.com>
2025-11-11 15:40:27 +1100

    2025-11-11_15:40:27

--- home-manager/modules/hypr.nix
+++ home-manager/modules/hypr.nix
@@ -164,16 +164,16 @@
       "$mainMod SHIFT, S, movetoworkspace, special:magic"
 
       "$mainMod, mouse_down, workspace, -1"
-      "$mainMod, mouse_down, exec, $unhide_waybar"
+      #"$mainMod, mouse_down, exec, $unhide_waybar"
 
       "$mainMod, mouse_up, workspace, +1"
-      "$mainMod, mouse_up, exec, $unhide_waybar"
+      #"$mainMod, mouse_up, exec, $unhide_waybar"
 
       "$mainMod SHIFT, mouse_down, movetoworkspace, -1"
-      "$mainMod SHIFT, mouse_down, exec, $unhide_waybar"
+      #"$mainMod SHIFT, mouse_down, exec, $unhide_waybar"
       
       "$mainMod SHIFT, mouse_up, movetoworkspace, +1"
-      "$mainMod SHIFT, mouse_up, exec, $unhide_waybar"
+      #"$mainMod SHIFT, mouse_up, exec, $unhide_waybar"
 
       "$mainMod, mouse_down, exec, $unhide_waybar"
 
