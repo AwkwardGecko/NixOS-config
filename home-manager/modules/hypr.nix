f7250f70d4ca7c4de58f7c04ea05b37377b2a1cd
Zozano <private@private.com>
2025-11-11 14:07:11 +1100

    2025-11-11_14:07:11

--- home-manager/modules/hypr.nix
+++ home-manager/modules/hypr.nix
@@ -35,7 +35,7 @@
     "$update_script" = "kitty bash ~/.dotfiles/system-update.sh";
 
     exec-once = [
-      "sleep 1 && openrgb --startminimized -p Default.orp.ba"
+      #"sleep 1 && openrgb --startminimized -p Default.orp.ba"
       "sleep 3 && signal-desktop"
       "sleep 1 && steam -silent"
       #"hypridle"
