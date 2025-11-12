c3b4e31844b2bb9eeebc53a766371464b20bcfc1
Zozano <private@private.com>
2025-11-12 12:17:15 +1100

    2025-11-12_12:17:14

--- home-manager/modules/hyprpanel.nix
+++ home-manager/modules/hyprpanel.nix
@@ -4,7 +4,27 @@
     enable = true;
     systemd.enable = true;
     settings = {
+      bar.battery.label = true;
+      bar.bluetooth.label = false;
       bar.clock.format = "%H:%M:%S";
+      bar.layouts = {
+        "*" = {
+          left = [
+            "dashboard"
+            "workspaces"
+            "media"
+          ];
+
+          middle = [ "windowtitle" ];
+
+          right = [
+            "volume"
+            "network"
+            "bluetooth"
+            "notifications"
+          ];
+        };
+      };
     };
   };
 }
