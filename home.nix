0ce31f5368592ec27e702ac6bda1e84172f8958d
Zozano <private@private.com>
2025-11-18 12:02:09 +1100

    2025-11-18_12:02:09

--- home.nix
+++ home.nix
@@ -69,8 +69,8 @@
 	home.file = {
     #".local/share/applications".source = source/local/share/applications;
     #".config/hypr/hypridle.conf".source = source/config/hypr/hypridle-xmrig-off.conf;
-    ".local/share/vlc/lua/extensions".source = source/local/share/vlc/lua/extensions;
-    ".config/waybar/gputemp.sh".source = source/config/waybar/gputemp.sh;
+    #".local/share/vlc/lua/extensions".source = source/local/share/vlc/lua/extensions;
+    #".config/waybar/gputemp.sh".source = source/config/waybar/gputemp.sh;
     # ".config/nsxiv/delete_and_next.sh".source = source/config/nsxiv/delete_and_next.sh;
     # ".config/nsxiv/exec/key-handler".source = source/config/nsxiv/exec/key-handler;
   };
