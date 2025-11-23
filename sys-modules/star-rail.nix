dcf8fd1a3d1602a445912f9d0539eaf7a4afc945
Zozano <private@private.com>
2025-11-23 13:25:34 +1100

    2025-11-23_13:25:34

--- sys-modules/star-rail.nix
+++ sys-modules/star-rail.nix
@@ -3,7 +3,7 @@ let
   aagl-gtk-on-nix = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz");
 in
 {
-  services.flatpak.enable = true;
+  # services.flatpak.enable = true;
     imports = [
     aagl-gtk-on-nix.module
   ];
