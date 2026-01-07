326054bd2c73d3e952f72b3c0efe3d58f6db3d96
Zozano <zozano@protonmail.com>
2026-01-07 14:35:24 +1100

    2026-01-07_14:35:24

--- sys-modules/hypr.nix
+++ sys-modules/hypr.nix
@@ -14,7 +14,7 @@
     xwayland.enable = true;
   };
 
-  services.windowManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
 
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk ];
