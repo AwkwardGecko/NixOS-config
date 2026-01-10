2b4f0c7bfcea5ab5e5d29e2970f7e7ae03446268
Zozano <zozano@protonmail.com>
2026-01-10 21:06:36 +1100

    2026-01-10_21:06:36

--- home-manager/source/config/waybar/gputemp.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/run/current-system/sw/bin/nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits
--- home-manager/source/local/share/applications/start.sh
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env bash
-#~/.local/share/applications/start.sh
-
-#sudo umount /mnt/luks
-sudo cryptsetup open /dev/disk/by-uuid/c5e87ce4-523f-46b9-8735-c6e7545a6d56 luks
-sudo mount /dev/mapper/luks /mnt/luks
-# bash /mnt/luks/stable-diffusion/webui.sh
-
-# bash webui.sh --medvram --ckpt /mnt/luks/models/Stable-diffusion/aZovyaPhotoreal_v2.safetensors
