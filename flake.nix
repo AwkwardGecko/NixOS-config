16456823263fd6a2d966476efdce877990440d49
Zozano <zozano@protonmail.com>
2026-02-07 10:13:48 +1100

    2026-02-07_10:13:48

--- flake.nix
+++ flake.nix
@@ -7,59 +7,35 @@
 
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
-
-    #comfyui.url = "github:utensils/nix-comfyui";
-    #comfyui.inputs.nixpkgs.follows = "nixpkgs";
-
-    #aagl.url = "github:ezKEa/aagl-gtk-on-nix";
-    #aagl.inputs.nixpkgs.follows = "nixpkgs"; # Name of nixpkgs input you want to use
-
   };
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      home-manager,
-      nixvim,
-      ...
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
+  outputs = inputs@{ self, nixpkgs, home-manager, nixvim, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations = {
+      z-nixos = nixpkgs.lib.nixosSystem {
         inherit system;
-        config.allowUnfree = true;
-      };
-    in
-    {
-      nixosConfigurations = {
-        z-nixos = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
-            nixvim.nixosModules.nixvim
-            #comfyui.nixosModules.default
-            {
-              #imports = [ aagl.nixosModules.default ];
-              #nix.settings = aagl.nixConfig;
-              # programs.honkers-railway-launcher.enable = true;
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          nixvim.nixosModules.nixvim
+          {
+            nixpkgs.config.allowUnfree = true;
 
-              environment = {
-                sessionVariables = {
-                  XDG_CURRENT_DESKTOP = "Hyprland";
-                  XDG_SESSION_TYPE = "wayland";
-                  XAUTHORITY = "\$HOME/.Xauthority";
-                };
-              };
+            environment.sessionVariables = {
+              XDG_CURRENT_DESKTOP = "Hyprland";
+              XDG_SESSION_TYPE = "wayland";
+            };
 
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.zozano = import ./home.nix;
-            }
-          ];
-        };
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.zozano = import ./home.nix;
+          }
+        ];
       };
     };
+  };
 }
+
---
+++ flakebackup/2026-02-07_flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
+    #comfyui.url = "github:utensils/nix-comfyui";
+    #comfyui.inputs.nixpkgs.follows = "nixpkgs";
+
+    #aagl.url = "github:ezKEa/aagl-gtk-on-nix";
+    #aagl.inputs.nixpkgs.follows = "nixpkgs"; # Name of nixpkgs input you want to use
+
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      nixvim,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      #pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      nixosConfigurations = {
+        z-nixos = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            nixvim.nixosModules.nixvim
+            {
+              environment = {
+                sessionVariables = {
+                  XDG_CURRENT_DESKTOP = "Hyprland";
+                  XDG_SESSION_TYPE = "wayland";
+                  #XAUTHORITY = "\$HOME/.Xauthority";
+                };
+              };
+
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.zozano = import ./home.nix;
+            }
+          ];
+        };
+      };
+    };
+}
