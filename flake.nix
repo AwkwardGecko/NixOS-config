0bf7c0cba954e1da1662bc37fd73258dabd50b5b
Zozano <zozano@protonmail.com>
2026-02-07 10:24:52 +1100

    2026-02-07_10:24:52

--- flake.nix
+++ flake.nix
@@ -11,20 +11,19 @@
 
   outputs = inputs@{ self, nixpkgs, home-manager, nixvim, ... }: {
     nixosConfigurations.z-nixos = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          nixvim.nixosModules.nixvim
-          {
-            nixpkgs.config.allowUnfree = true;
-
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.zozano = import ./home.nix;
-          }
-        ];
-      };
+	    system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        nixvim.nixosModules.nixvim
+        {
+          nixpkgs.config.allowUnfree = true;
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.zozano = import ./home.nix;
+        }
+      ];
+    };
   };
 }
