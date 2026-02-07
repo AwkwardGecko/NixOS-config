fb0599f1d58dacb9a04153b92c9e15e2859352fb
Zozano <zozano@protonmail.com>
2026-02-07 11:03:50 +1100

    2026-02-07_11:03:50

--- flake.nix
+++ flake.nix
@@ -7,17 +7,23 @@
 
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
+    comfyui-nix.url = "github:utensils/comfyui-nix";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nixvim, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, nixvim, comfyui-nix, ... }: {
     nixosConfigurations.z-nixos = nixpkgs.lib.nixosSystem {
 	    system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
+        
         ./configuration.nix
         home-manager.nixosModules.home-manager
         nixvim.nixosModules.nixvim
         {
+          nixpkgs.overlays = [ comfyui-nix.overlays.default ];
+          environment.systemPackages = [ pkgs.comfy-ui ];
+
           nixpkgs.config.allowUnfree = true;
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
