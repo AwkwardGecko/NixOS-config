292f581e76befc66959a2cbdb7cd077c506a0312
Zozano <zozano@protonmail.com>
2026-02-14 12:13:56 +1100

    2026-02-14_12:13:56

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -19,7 +19,8 @@
 
     nvidia = {
       modesetting.enable = true;
-      open = true;
+      #open = true;
+      open = false;
       nvidiaSettings = true;
       powerManagement.enable = false;
       powerManagement.finegrained = false;
