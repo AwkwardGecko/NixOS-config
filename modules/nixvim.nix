3ee5560e2452d148b4b1e1501b20d05bf12a47cd
Zozano <zozano@protonmail.com>
2026-02-25 09:42:06 +1100

    2026-02-25_09:42:06

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -3,28 +3,47 @@
   programs.nixvim = {
     enable = true;
     colorschemes.gruvbox.enable = true;
+    
+    extraPackages = [
+      pkgs.tree-sitter
+      pkgs.nil
+    ];
+
+
     opts = {
     	tabstop = 2;
-	shiftwidth = 2;
-	softtabstop = 2;
-	smartindent = true;
-	expandtab = true;
-	number = true;
-	relativenumber = true;
-	wrap = false;
-	clipboard = "unnamedplus";
+	    shiftwidth = 2;
+	    softtabstop = 2;
+	    smartindent = true;
+	    expandtab = true;
+	    number = true;
+	    relativenumber = true;
+	    wrap = false;
+	    clipboard = "unnamedplus";
     };
+
+    extraConfigLuaPre = ''
+      vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/site')
+    '';
+
     plugins = {
+      
       lualine.enable = true;
-      treesitter.enable = true;
       comment.enable = true;
       nvim-autopairs.enable = true;
-    };
+      
+      treesitter = {
+        enable = true;
+        nixGrammars = true;
+      };
+  
     lsp = {
     	enable = true;
-	servers = {
-		nil_ls.enable = true;
-	};
+	    autoLoad = true;
+
+      servers = {
+		    nil_ls.enable = true;
+	    };
     };
   };
 }
