ccd332964492d2301e7afeccd11dd74f044523e4
Zozano <zozano@protonmail.com>
2026-02-25 10:42:15 +1100

    2026-02-25_10:42:15

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -3,7 +3,7 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-    base24Scheme = "${pkgs.base24-schemes}/share/themes/gruvbox-dark.yaml";
+    #base24Scheme = "${pkgs.base24-schemes}/share/themes/gruvbox-dark.yaml";
 
     #image = pkgs.fetchurl {
     #  url = "https://getwallpapers.com/wallpaper/full/1/4/3/523784.jpg";
