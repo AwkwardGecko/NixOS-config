c13fccdd7f4a98c09d9c33d0e7d2437936b3438c
Zozano <zozano@protonmail.com>
2026-02-25 12:03:04 +1100

    2026-02-25_12:03:04

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -28,10 +28,10 @@
       };
     };
 
-      cursor = {
-        package = pkgs.bibata-cursors;
-        name = "Bibata-Modern-Ice";
-      };
+      #cursor = {
+      #  package = pkgs.bibata-cursors;
+      #  name = "Bibata-Modern-Ice";
+      #};
 
     #targets.hyperpanel.enable = false;
 
