4652b0e561633f8d2a3335236f7cbea5b1eb7980
Zozano <zozano@protonmail.com>
2026-02-25 11:49:17 +1100

    2026-02-25_11:49:17

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -3,7 +3,8 @@
   stylix = {
     enable = true;
     
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+
     
     fonts = {
       serif = {
