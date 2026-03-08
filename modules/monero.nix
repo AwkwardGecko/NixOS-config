343e7d4910c1f9e4048aa4496633b68d375ecd4b
Zozano <zozano@protonmail.com>
2026-03-08 12:13:17 +1100

    2026-03-08_12:13:17

--- modules/monero.nix
+++ modules/monero.nix
@@ -1,31 +1,31 @@
+{ config, lib, pkgs, ... }:
 {
-  lib,
-  pkgs,
-  config,
-  ...
-}:
-
-{
-  # # Enable Monero service (for node management)
-  # services.monero = {
-  #   enable = true;
-  #   dataDir = "/steam/Monero";
-  #
-  #
-  #
-  #   #mining.enable = true;
-  #   #mining.address = "48rmufMfAAiHH4N8q7wzdrdvcN7AXcgwTN2oEqCrCnBafCeyFaZNjZbG6ytK4BsnpUZnLuRMAstaeSpDs3JKg4qrT3x1K2K";  # Replace with your Monero wallet address
-  #   #mining.threads = 6;  # Adjust based on how many threads you want to allocate for mining
-  # };
-
   environment.systemPackages = with pkgs; [
     monero-gui
     monero-cli
-  ];
+  ]; 
+
+  services.monero = {
+    enable = true;
+    prune = true;
+    dataDir = "/monero";
+
+    limit.threads = 8;
+    limit.syncSize = 20;
+
+    extraConfig = ''
+      out-peers=64
+      enable-dns-blocklist=1
+      db-sync-mode-fast:async:1000
+    '';
+  
+   };
+
+
 
   networking.firewall.allowedTCPPorts = [
     18080
-    18081
+    #18081
   ];
 
   # users.users.zozano = {
@@ -36,23 +36,22 @@
   #   createHome = true;
   # };
 
-  systemd.services.monero = {
-    description = "monero daemon";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      User = "zozano";
-      Group = "users";
-      ExecStart = "${pkgs.monero-cli}/bin/monerod --config-file=/home/zozano/.bitmonero/monero.conf --non-interactive --data-dir=/home/zozano/.bitmonero --out-peers 64 --prune-blockchain --enable-dns-blocklist --max-concurrency 8 --block-sync-size 20 --db-sync-mode fast:async:1000";
-      ExecStop = "${pkgs.monero-cli}/bin/monerod exit";
-      TimeoutStopSec = "90s";
-      Restart = "always";
-      SuccessExitStatus = [
-        0
-        1
-      ];
-    };
-  };
-
+  # systemd.services.monero = {
+  #   description = "monero daemon";
+  #   after = [ "network.target" ];
+  #   wantedBy = [ "multi-user.target" ];
+  #
+  #   serviceConfig = {
+  #     User = "zozano";
+  #     Group = "users";
+  #     ExecStart = "${pkgs.monero-cli}/bin/monerod --config-file=/home/zozano/.bitmonero/monero.conf --non-interactive --data-dir=/home/zozano/.bitmonero --out-peers 64 --prune-blockchain --enable-dns-blocklist --max-concurrency 8 --block-sync-size 20 --db-sync-mode fast:async:1000";
+  #     ExecStop = "${pkgs.monero-cli}/bin/monerod exit";
+  #     TimeoutStopSec = "90s";
+  #     Restart = "always";
+  #     SuccessExitStatus = [
+  #       0
+  #       1
+  #     ];
+  #   };
+  # };
 }
