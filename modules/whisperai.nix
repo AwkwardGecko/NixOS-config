10316eafee0000547f96295f76062f583082cdc7
Zozano <zozano@protonmail.com>
2026-03-12 17:35:05 +1100

    2026-03-12_17:35:05

--- modules/whisperai.nix
+++ modules/whisperai.nix
@@ -9,4 +9,11 @@
   environment.systemPackages = with pkgs; [
     python314Packages.openai-whisper
   ];
+
+  nix.settings = {
+    substituters = [ "https://cache.nixos-cuda.org" ];
+    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+  };
+
+  nixpkgs.config.cudaSupport = true;
 }
