669b9231b2a6b24d7b672de1b6853a7f07f7ae0f
Zozano <zozano@protonmail.com>
2026-03-12 11:47:21 +1100

    2026-03-12_11:47:21

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -38,7 +38,7 @@
     #   size = 16;
     # };
 
-    targets.hyprpanel.enable = false;
+    targets.hyperpanel.enable = false;
 
     #image = pkgs.fetchurl {
     #  url = "https://getwallpapers.com/wallpaper/full/1/4/3/523784.jpg";
