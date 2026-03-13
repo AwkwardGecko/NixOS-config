4650161d7006163525332eea48c19a8066b204d7
Zozano <zozano@protonmail.com>
2026-03-14 10:40:46 +1100

    2026-03-14_10:40:46

--- modules/whisperai.nix
+++ modules/whisperai.nix
@@ -7,16 +7,13 @@
 
 {
   environment.systemPackages = with pkgs; [
-    ffmpeg
-    (python312Packages.openai-whisper.override {
-      torch = python312Packages.torch-bin;
-     })
+    python312Packages.openai-whisper
   ];
 
-  nix.settings = {
-    substituters = [ "https://cache.nixos-cuda.org" ];
-    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
-  };
+  #nix.settings = {
+  #  substituters = [ "https://cache.nixos-cuda.org" ];
+  #  trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+  #};
 
   #nixpkgs.config = {
     #cudaSupport = true;
