5a224b5e9fb715ae604c78a5f12b0e57a0f9c5d8
Zozano <zozano@protonmail.com>
2026-03-16 16:58:23 +1100

    2026-03-16_16:58:23

--- modules/security.nix
+++ modules/security.nix
@@ -5,6 +5,10 @@
   ...
 }:
 {
+  environment.systemPackages = with pkgs; [
+    seahorse #keyring
+  ];
+
   services.gnome.gnome-keyring.enable = true;
   services.gnome.gcr-ssh-agent.enable = false;
 
