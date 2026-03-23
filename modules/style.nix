e0f07cf8539e7b98f34c9647c2558221d37e2d95
Zozano <zozano@protonmail.com>
2026-03-23 12:22:17 +1100

    2026-03-23_12:22:17

--- modules/style.nix
+++ modules/style.nix
@@ -11,31 +11,8 @@
 
 {
   home.pointerCursor = {
-    #gtk = {
-      #enable = true;
-      #gtk4.theme = null;
-      # x11.enable = true;
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 16;
-    #};
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
   };
-
-  # gtk = {
-  #   enable = true;
-  #
-  #   theme = {
-  #     package = pkgs.flat-remix-gtk;
-  #     name = "Flat-Remix-GTK-Grey-Darkest";
-  #   };
-  #
-  #   iconTheme = {
-  #     package = pkgs.papirus-icon-theme;
-  #     name = "Adwaita";
-  #   };
-  #
-  #   font = {
-  #     name = "Sans";
-  #     size = 11;
-  #   };
 }
