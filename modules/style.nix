7647db3471ba79268b3d87b32ce1a57b5b5d5c59
Zozano <zozano@protonmail.com>
2026-03-23 12:10:36 +1100

    2026-03-23_12:10:36

--- modules/style.nix
+++ modules/style.nix
@@ -11,11 +11,14 @@
 
 {
   home.pointerCursor = {
-    gtk.enable = true;
-    # x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 16;
+    gtk = {
+      enable = true;
+      gtk4.theme = null;
+      # x11.enable = true;
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 16;
+    };
   };
 
   # gtk = {
