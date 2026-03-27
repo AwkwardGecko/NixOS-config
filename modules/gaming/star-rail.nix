cac7a11c23ab9b1cbfe816674fad5bcd45c50b26
Zozano <zozano@protonmail.com>
2026-03-28 10:34:37 +1100

    2026-03-28_10:34:37

--- modules/gaming/star-rail.nix
+++ modules/gaming/star-rail.nix
@@ -22,7 +22,7 @@
   home-manager.users.zozano = {
     xdg.desktopEntries."moe.launcher.the-honkers-railway-launcher" = {
       name = "Honkai: Star Rail";
-      exec = "sflatpak run --branch=stable --arch=x86_64 --command=moe.launcher.the-honkers-railway-launcher moe.launcher.the-honkers-railway-launcher";
+      exec = "flatpak run --branch=stable --arch=x86_64 --command=moe.launcher.the-honkers-railway-launcher moe.launcher.the-honkers-railway-launcher";
       icon = "moe.launcher.the-honkers-railway-launcher";
       comment = "Honkai: Star Rail";
       categories = [ "Game" ];
