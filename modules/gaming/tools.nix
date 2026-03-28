73c029d28fb921a1ddbe58ed92e352c18e955019
Zozano <zozano@protonmail.com>
2026-03-28 12:38:42 +1100

    2026-03-28_12:38:42

--- modules/gaming/tools.nix
+++ modules/gaming/tools.nix
@@ -26,22 +26,22 @@
       cudaPackages = pkgs.cudaPackages;
     };
       
-    applications.apps = [
-      {
-        name = "Honkai: Star Rail";
-        #cmd = "launch-hsr";
-        #cmd = "flatpak run --branch=stable --arch=x86_64 --command=moe.launcher.the-honkers-railway-launcher moe.launcher.the-honkers-railway-launcher";
-        cmd = "HSR-skip-launcher";
-        image-path = "/home/zozano/.config/sunshine/covers/igdb_394848.png";
-        auto-detach = "true";
-      }
-      {
-        name = "Steam Big Picture";
-        #cmd = "steam steam://open/bigpicture";
-        cmd = "sleep 5 && steam -bigpicture";
-        image-path = "/home/zozano/.config/sunshine/covers/steam.png";
-        auto-detach = "true";
-      }
-    ];
+    # applications.apps = [
+    #   {
+    #     name = "Honkai: Star Rail";
+    #     #cmd = "launch-hsr";
+    #     #cmd = "flatpak run --branch=stable --arch=x86_64 --command=moe.launcher.the-honkers-railway-launcher moe.launcher.the-honkers-railway-launcher";
+    #     cmd = "HSR-skip-launcher";
+    #     image-path = "/home/zozano/.config/sunshine/covers/igdb_394848.png";
+    #     auto-detach = "true";
+    #   }
+    #   {
+    #     name = "Steam Big Picture";
+    #     #cmd = "steam steam://open/bigpicture";
+    #     cmd = "sleep 5 && steam -bigpicture";
+    #     image-path = "/home/zozano/.config/sunshine/covers/steam.png";
+    #     auto-detach = "true";
+    #   }
+    # ];
   };
 }
