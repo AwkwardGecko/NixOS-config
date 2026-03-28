3e2f70363f8f17ffac97e896358f6a1c0be056a9
Zozano <zozano@protonmail.com>
2026-03-28 11:36:11 +1100

    2026-03-28_11:36:11

--- modules/gaming/tools.nix
+++ modules/gaming/tools.nix
@@ -5,12 +5,12 @@
     goverlay
     wine
     # unigine-superposition - don't use. run .exe through steam for Vulkan support
-    (writeShellScriptBin "sunshine-run" ''
-      export WAYLAND_DISPLAY="''${WAYLAND_DISPLAY:-wayland-1}"
-      export XDG_RUNTIME_DIR="''${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
-      export DISPLAY="''${DISPLAY:-:0}"
-      exec "$@"
-    '')
+    # (writeShellScriptBin "sunshine-run" ''
+    #   export WAYLAND_DISPLAY="''${WAYLAND_DISPLAY:-wayland-1}"
+    #   export XDG_RUNTIME_DIR="''${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
+    #   export DISPLAY="''${DISPLAY:-:0}"
+    #   exec "$@"
+    # '')
   ];
 
   hardware.uinput.enable = true;
@@ -26,21 +26,21 @@
       cudaPackages = pkgs.cudaPackages;
     };
       
-    applications.apps = [
-      {
-        name = "Honkai: Star Rail";
-        #cmd = "launch-hsr";
-        #cmd = "flatpak run --branch=stable --arch=x86_64 --command=moe.launcher.the-honkers-railway-launcher moe.launcher.the-honkers-railway-launcher";
-        cmd = "/run/current-system/sw/bin/sunshine-run HSR-skip-launcher";
-        image-path = "/home/zozano/.config/sunshine/covers/igdb_394848.png";
-        auto-detach = "true";
-      }
-      {
-        name = "Steam Big Picture";
-        cmd = "/run/current-system/sw/bin/sunshine-run steam steam://open/bigpicture";
-        image-path = "/home/zozano/.config/sunshine/covers/steam.png";
-        auto-detach = "true";
-      }
-    ];
+    # applications.apps = [
+    #   {
+    #     name = "Honkai: Star Rail";
+    #     #cmd = "launch-hsr";
+    #     #cmd = "flatpak run --branch=stable --arch=x86_64 --command=moe.launcher.the-honkers-railway-launcher moe.launcher.the-honkers-railway-launcher";
+    #     cmd = "/run/current-system/sw/bin/sunshine-run HSR-skip-launcher";
+    #     image-path = "/home/zozano/.config/sunshine/covers/igdb_394848.png";
+    #     auto-detach = "true";
+    #   }
+    #   {
+    #     name = "Steam Big Picture";
+    #     cmd = "/run/current-system/sw/bin/sunshine-run steam steam://open/bigpicture";
+    #     image-path = "/home/zozano/.config/sunshine/covers/steam.png";
+    #     auto-detach = "true";
+    #   }
+    # ];
   };
 }
