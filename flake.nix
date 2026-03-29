86080e587577b6db9f4b4cf6864e7b67d02f5edd
Zozano <zozano@protonmail.com>
2026-03-29 13:54:43 +1100

    2026-03-29_13:54:43

--- flake.nix
+++ flake.nix
@@ -22,8 +22,8 @@
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, nix-flatpak, nixvim, comfyui-nix, stylix, ... }: {
-    #nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-    nixosConfigurations.z-nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+    #nixosConfigurations.z-nixos = nixpkgs.lib.nixosSystem {
       # Build name only - runtime hostname derived dynamically in ./modules/networking/hostname.nix
 	    #system = "x86_64-linux";
       specialArgs = { inherit inputs; };
