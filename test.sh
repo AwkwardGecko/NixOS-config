40af9b096f8adbfb08a0909a75d9f21027842387
Zozano <zozano@protonmail.com>
2026-03-30 20:55:04 +1100

    2026-03-30_20:55:04

--- test.sh
+++
@@ -1,6 +0,0 @@
-cd "$HOME/.local/share/Steam/steamapps/common/Borderlands 2" || exit
-mkdir -p lib.bak
-for f in lib/libcurl.so* lib/libssl.so* lib/libcrypto.so*; do
-  [ -e "$f" ] && mv -v "$f" lib.bak/
-done
-
