d0c66c993035537f2943915516b08b444d9379de
Zozano <zozano@protonmail.com>
2026-04-06 12:52:24 +1000

    2026-04-06_12:52:24

--- modules/desktop/portals.nix
+++ modules/desktop/portals.nix
@@ -20,19 +20,22 @@
   ];
 
   home-manager.users.zozano = {
+    programs.mpv.enable = true;
     xdg.mimeApps = {
       enable = true;
       defaultApplications = {
         "text/html" = "firefox.desktop";
         "x-scheme-handler/http" = "firefox.desktop";
         "x-scheme-handler/https" = "firefox.desktop";
-        "image/png" = "imv.desktop";
-        "image/jpeg" = "imv.desktop";
-        "image/gif" = "nomacs.desktop";
-        "video/mp4" = "mpv.desktop";
-        "video/x-matroska" = "mpv.desktop";
+        "image/*" = "nomacs.desktop";
+        #"image/png" = "imv.desktop";
+        #"image/jpeg" = "imv.desktop";
+        #"image/gif" = "nomacs.desktop";
+        "video/*" = "vlc.desktop";
+        #"video/mp4" = "mpv.desktop";
+        #"video/x-matroska" = "mpv.desktop";
         "application/pdf" = "org.pwmt.zathura.desktop";
-        "text/plain" = "gedit.desktop";
+        "text/*" = "gedit.desktop";
         "inode/directory" = ["nautilus.desktop"];
       };
     };
