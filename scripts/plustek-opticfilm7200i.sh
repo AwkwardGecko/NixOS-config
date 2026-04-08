034b1017ebfde60fe1f2f29246dd565025756f05
Zozano <zozano@protonmail.com>
2026-04-08 18:05:39 +1000

    2026-04-08_18:05:39

--- scripts/plustek-opticfilm7200i.sh
+++ scripts/plustek-opticfilm7200i.sh
@@ -3,7 +3,12 @@ cd ~/Proton-Drive
 scanimage --device 'genesys:libusb:003:005' \
   --mode Color \
   --source "Transparency Adapter" \
-  --resolution 7200 \
-  --depth 16 \
-  --format=tiff \
-  -o scan.tiff
+  --resolution 3600 \ # half res
+  --format=png \
+  -o scan_$(date +%Y-%m-%d_%H_%M_%S).png
+
+  # super high def
+  # --resolution 7200 \
+  # --depth 16 \
+  # --format=tiff \
+  # -o scan_$(date +%Y-%m-%d_%H_%M_%S).png
