f2c9a19880d494fece090b58be921d149eddf767
Zozano <zozano@protonmail.com>
2026-04-08 13:42:46 +1000

    2026-04-08_13:42:46

--- modules/gaming/reliquary-archiver.nix
+++ modules/gaming/reliquary-archiver.nix
@@ -4,17 +4,26 @@
   pkgs,
   ...
 }: {
-  home-manager.users.zozano.home.file."reliquary-archiver/default.nix".text = ''
-    { pkgs ? import <nixpkgs> {} }:
+  home-manager.users.zozano.home.file = {
+    "reliquary-archiver/default.nix".text = ''
+      { pkgs ? import <nixpkgs> {} }:
 
-    pkgs.mkShell {
-      buildInputs = with pkgs; [
-        cargo
-        pkg-config
-        libpcap
-        wayland
-        tcpdump
-      ];
-    }
-  '';
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          pkg-config
+          libpcap
+          wayland
+          tcpdump
+        ];
+      }
+    '';
+    "reliquary-archiver/build.sh".text = ''
+      #!/usr/bin/env bash
+      cd reliquary-archiver-*
+      cargo build --release
+      sudo setcap CAP_NET_RAW=+ep target/release/reliquary-archiver
+      cargo run
+    '';
+  };
 }
