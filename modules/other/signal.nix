40a0ac04f05605253b65c5fcadfdc9cbe23d34a4
Zozano <zozano@protonmail.com>
2026-04-11 12:12:41 +1000

    2026-04-11_12:12:41

--- modules/other/signal.nix
+++ modules/other/signal.nix
@@ -7,31 +7,30 @@
   cfg = config.services.signal-read-notify;
   script = ./signal-read-notify.py;
 in {
-
-home-manager.users.zozano = {
-  options.services.signal-read-notify = {
-    enable = lib.mkEnableOption "Signal read receipt desktop notifications";
-    pollInterval = lib.mkOption {
-      type = lib.types.int;
-      default = 3;
-      description = "Polling interval in seconds.";
-    };
-  };
-
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      signal-desktop
-      signal-export
-    ];
-
-    systemd.user.services.signal-read-notify = {
-      # ...
+  home-manager.users.zozano = {
+    services.signal-read-notify = {
+      enable = lib.mkEnableOption "Signal read receipt desktop notifications";
+      pollInterval = lib.mkOption {
+        type = lib.types.int;
+        default = 3;
+        description = "Polling interval in seconds.";
+      };
     };
 
-    home.packages = with pkgs; [
-      libnotify
-      sqlcipher
-    ];
+    config = lib.mkIf cfg.enable {
+      environment.systemPackages = with pkgs; [
+        signal-desktop
+        signal-export
+      ];
+
+      systemd.user.services.signal-read-notify = {
+        # ...
+      };
+
+      home.packages = with pkgs; [
+        libnotify
+        sqlcipher
+      ];
+    };
   };
-};
 }
