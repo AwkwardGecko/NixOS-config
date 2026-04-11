381c94854a133775ef265930ce1e7697314a5438
Zozano <zozano@protonmail.com>
2026-04-11 12:15:24 +1000

    2026-04-11_12:15:24

--- modules/other/signal.nix
+++ modules/other/signal.nix
@@ -7,12 +7,6 @@
   cfg = config.services.signal-read-notify;
   script = ./signal-read-notify.py;
 in {
-  environment.systemPackages = with pkgs; [
-    signal-desktop
-    signal-export
-  ];
-
-  home-manager.users.zozano = {
     options.services.signal-read-notify = {
       enable = lib.mkEnableOption "Signal read receipt desktop notifications";
       pollInterval = lib.mkOption {
@@ -22,6 +16,13 @@ in {
       };
     };
     config = lib.mkIf cfg.enable {
+      environment.systemPackages = with pkgs; [
+        signal-desktop
+        signal-export
+      ];
+
+
+
       systemd.user.services.signal-read-notify = {
         Unit = {
           Description = "Signal read receipt notifier";
