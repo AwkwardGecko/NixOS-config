1497c2214ef594c54dc823dc1d29ee383770fca0
Zozano <zozano@protonmail.com>
2026-04-21 11:35:27 +1000

    2026-04-21_11:35:27

--- modules/gaming/reliquary-archiver.nix
+++ modules/gaming/reliquary-archiver.nix
@@ -22,7 +22,7 @@
       "Games/reliquary-archiver/build.sh" = {
         text = ''
           #!/usr/bin/env bash
-          cd Games/reliquary-archiver/reliquary-archiver*
+          cd ~/Games/reliquary-archiver/reliquary-archiver*
           rm archive_output-*
           cargo build --release
           #sudo setcap CAP_NET_RAW=+ep target/release/reliquary-archiver
