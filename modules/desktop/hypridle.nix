f1f237c2522961fbedcf1bdf6a529a39f698789c
Zozano <zozano@protonmail.com>
2026-04-28 19:47:44 +1000

    2026-04-28_19:47:44

--- modules/desktop/hypridle.nix
+++ modules/desktop/hypridle.nix
@@ -24,18 +24,6 @@
             bezier = "easeOutQuint";
           };
         };
-
-        input-field = [
-          {
-            size = "200, 50";
-            position = "0, -80";
-            monitor = "";
-            dots_center = true;
-            fade_on_empty = false;
-            outline_thickness = 5;
-            shadow_passes = 2;
-          }
-        ];
       };
     };
 
