ea1683ad30f53d538be545dbd92bdfcfd70b33c3
Zozano <zozano@protonmail.com>
2026-05-10 11:09:18 +1000

    2026-05-10_11:09:18

--- modules/gaming/tools.nix
+++ modules/gaming/tools.nix
@@ -8,6 +8,7 @@
     #bottles
     goverlay
     wine
+    starship-sf64
     # unigine-superposition - don't use. run .exe through steam for Vulkan support
     # (writeShellScriptBin "sunshine-run" ''
     #   export WAYLAND_DISPLAY="''${WAYLAND_DISPLAY:-wayland-1}"
