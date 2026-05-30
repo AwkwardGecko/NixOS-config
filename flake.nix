5cca5eddc00da8f44c3bbb1472b6747c9ab45c4e
Zozano <zozano@protonmail.com>
2026-05-30 15:15:41 +1000

    2026-05-30_15:15:41

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     stylix.url = "github:nix-community/stylix/master";
-    stylix.inputs.home-manager.follows = "home-manager";
+    #stylix.inputs.home-manager.follows = "home-manager";
   };
 
   outputs = inputs @ {
