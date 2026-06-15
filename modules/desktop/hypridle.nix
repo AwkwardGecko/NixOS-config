ee3c453020382d1e490673f82653b73db70ab428
Zozano <zozano@protonmail.com>
2026-06-15 13:32:40 +1000

    2026-06-15_13:32:40

--- modules/desktop/hypridle.nix
+++ modules/desktop/hypridle.nix
@@ -29,12 +29,12 @@
           {
             timeout = 30;
             on-timeout = "hyprctl dispatch dpms off";
-          #on-resume = "hyprctl dispatch dpms on && hyprctl reload";
+            #on-resume = "hyprctl dispatch dpms on && hyprctl reload";
             on-resume = "hyprctl dispatch dpms on";
           }
 
           {
-            timeout = 600;
+            timeout = 40;
             on-timeout = "systemctl suspend";
           }
         ];
