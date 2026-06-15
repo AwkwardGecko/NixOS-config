68c744269f447307bbf78f4c77b2b7e40d798930
Zozano <zozano@protonmail.com>
2026-06-15 13:05:23 +1000

    2026-06-15_13:05:23

--- modules/desktop/hypridle.nix
+++ modules/desktop/hypridle.nix
@@ -5,7 +5,6 @@
   ...
 }: {
   home-manager.users.zozano = {
-    
     programs.hyprlock = {
       enable = true;
       settings = {
@@ -20,9 +19,9 @@
       enable = true;
       settings = {
         general = {
-          #lock_cmd = "pidof hyprlock || hyprlock";
-          #before_sleep_cmd = "loginctl lock-session";
-          after_sleep_cmd = "hyprctl dispatch dpms on; hyprctl keyword misc:allow_session_lock_restore 1";
+          lock_cmd = "pidof hyprlock || hyprlock";
+          before_sleep_cmd = "loginctl lock-session";
+          after_sleep_cmd = "hyprctl dispatch dpms on";
           ignore_dbus_inhibit = false;
         };
 
@@ -30,7 +29,8 @@
           {
             timeout = 300;
             on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on && hyprctl reload";
+            #on-resume = "hyprctl dispatch dpms on && hyprctl reload";
+            on-resume = "hyprctl dispatch dpms on";
           }
 
           {
