61845d14e85d32ff496b12342b08bc6f91436b1a
Zozano <zozano@protonmail.com>
2026-06-21 01:24:48 +1000

    2026-06-21_01:24:48

--- modules/other/kde-connect.nix
+++ modules/other/kde-connect.nix
@@ -4,6 +4,9 @@
   pkgs,
   ...
 }: {
+
+  home-manager.users.zozano.home.sesstionVariables.YDOTOOL_DOCKER = "/run/ydotoold/socket";
+
   programs = {
     kdeconnect.enable = true;
     ydotool.enable = true;
