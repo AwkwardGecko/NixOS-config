7905af43c3dbe3fda718a9f05593785a2220034d
Zozano <zozano@protonmail.com>
2026-06-21 16:46:59 +1000

    2026-06-21_16:46:59

--- flake.nix
+++ flake.nix
@@ -47,6 +47,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.zozano = import ./home.nix;
+          home-manager.extraSpecialArgs = { inherit inputs; };
         }
       ];
     };
