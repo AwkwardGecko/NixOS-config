c71b15ea52503931e33ad376e35bb8c64b92c64d
Zozano <zozano@protonmail.com>
2026-06-27 18:34:56 +1000

    2026-06-27_18:34:56

--- modules/gaming/controller.nix
+++ modules/gaming/controller.nix
@@ -11,4 +11,17 @@
     xone.enable = true; # dongle support
     #xpadneo.enable = true; # bluetooth support
   };
+
+  boot.blacklistedKernelModules = [ "xone_dongle" ]; # delayed load to prevent boot fault
+
+  systemd.user.services.load-xone-dongle = {
+    description = "Load Xbox dongle driver after login";
+    wantedBy = [ "graphical.target" ];
+    after = [ "graphical.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 60";
+      ExecStart = "${pkgs.kmod}/bin/modprobe xone_dongle";
+    };
+  };
 }
