7e4c5ff604191238e88e55b9c878f48161eaba7b
Zozano <zozano@protonmail.com>
2026-07-09 09:54:26 +1000

    2026-07-09_09:54:26

--- modules/desktop/style.nix
+++ modules/desktop/style.nix
@@ -9,6 +9,7 @@
 }: {
   home-manager.users.zozano = {
     home.pointerCursor = {
+      enable = true;
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Classic";
       size = 16;
