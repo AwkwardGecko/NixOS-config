5ccb9291c424b57dfd60eceaeca2da4a653b0762
Zozano <zozano@protonmail.com>
2026-07-10 20:03:45 +1000

    2026-07-10_20:03:45

--- configuration.nix
+++ configuration.nix
@@ -24,11 +24,11 @@
     ./modules/core/rgb.nix
 
     ./modules/desktop/hyprland.nix
-    #./modules/desktop/wayle.nix
+    ./modules/desktop/wayle.nix
     ./modules/desktop/cursor.nix
     ./modules/desktop/hypridle.nix
     ./modules/desktop/rofi.nix
-    #./modules/desktop/stylix.nix
+    ./modules/desktop/stylix.nix
     ./modules/desktop/theme.nix
     ./modules/desktop/wallpaper.nix
     ./modules/desktop/portals.nix
