4d92e99149249c97b1273b24a4e3f30376de4adb
Zozano <zozano@protonmail.com>
2026-07-15 09:37:55 +1000

    2026-07-15_09:37:55

---
+++ fix.sh
@@ -0,0 +1,6 @@
+ln -sf ~/.linuxbrew/opt/kitty/share/applications/kitty.desktop \
+       ~/.local/share/applications/kitty.desktop
+ln -sf ~/.linuxbrew/opt/kitty/share/applications/kitty-open.desktop \
+       ~/.local/share/applications/kitty-open.desktop
+ln -sf ~/.linuxbrew/opt/kitty/share/icons/hicolor/256x256/apps/kitty.png \
+       ~/.local/share/icons/hicolor/256x256/apps/kitty.png
---
+++ test.sh
@@ -0,0 +1,5 @@
+cat /etc/default/grub
+which os-prober
+rpm -q grub2-tools os-prober 2>/dev/null
+ls -la /boot/grub2/
+cat /boot/grub2/grub.cfg | head -50
