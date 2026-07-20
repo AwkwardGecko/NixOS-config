63a78aea5d92c9de6dc6671821792285544f63c2
Zozano <zozano@protonmail.com>
2026-07-20 10:23:40 +1000

    2026-07-20_10:23:40

--- modules/other/rustdesk.nix
+++ modules/other/rustdesk.nix
@@ -1,11 +1,15 @@
-{ config, lib, pkgs, ... }:
 {
-  environment.systemPackages = [ pkgs.rustdesk ];
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = [pkgs.rustdesk];
 
   systemd.services.rustdesk = {
     description = "RustDesk unattended access";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
+    wantedBy = ["multi-user.target"];
+    after = ["network.target"];
     serviceConfig = {
       ExecStart = "${pkgs.rustdesk}/bin/rustdesk --service";
       Restart = "always";
@@ -14,7 +18,7 @@
   };
 
   networking.firewall = {
-    allowedTCPPorts = [ 21115 21116 21117 21118 21119 ];
-    allowedUDPPorts = [ 21116 ];
+    allowedTCPPorts = [21115 21116 21117 21118 21119];
+    allowedUDPPorts = [21116];
   };
 }
