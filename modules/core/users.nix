b05163dd9babdd354cb3621f8fca380c9e1ad0d6
Zozano <zozano@protonmail.com>
2026-08-16 14:22:24 +1000

    2026-08-16_14:22:24

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -4,17 +4,32 @@
   lib,
   ...
 }: {
-  users.users.zozano = {
-    isNormalUser = true;
-    description = "zozano";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "gamemode"
-      "docker"
-      "input"
-      "uinput"
-      "ydotool"
-    ];
+  users.users = {
+    zozano = {
+      isNormalUser = true;
+      description = "zozano";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "gamemode"
+        "docker"
+        "input"
+        "uinput"
+        "video"
+        "audio"
+        "ydotool"
+      ];
+    };
+    leo = {
+      isNormalUser = true;
+      description = "leo";
+      extraGroups = [
+        "networkmanager"
+        "video"
+        "input"
+        "wheel"
+        "audio"
+      ];
+    };
   };
 }
