3f9df31bb577a786fe285a2985bc1de69e31c496
Zozano <zozano@protonmail.com>
2026-08-26 09:55:28 +1000

    2026-08-26_09:55:28

--- modules/other/signal/signal.nix
+++ modules/other/signal/signal.nix
@@ -18,13 +18,13 @@
   home-manager.users.zozano.systemd.user.services.signal-desktop = {
     Unit = {
       Description = "Signal Autostart";
-      PartOf = [ "graphical-session.target" ];
-      After = [ "graphical-session.target" ];
+      PartOf = ["graphical-session.target"];
+      After = ["graphical-session.target"];
     };
     Service = {
       ExecStart = "${pkgs.signal-desktop}/bin/signal-desktop";
       Restart = "on-failure";
     };
-    Install.WantedBy = [ "graphical-session.target" ];
+    Install.WantedBy = ["graphical-session.target"];
   };
 }
