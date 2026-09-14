775479758e6cf2537585e4af0b998d7aa0d2f362
Zozano <zozano@protonmail.com>
2026-09-14 11:11:40 +1000

    2026-09-14_11:11:40

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -7,40 +7,53 @@
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
+    extest.enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+    extraCompatPackages = [pkgs.proton-ge-bin];
   };
 
-  environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/zozano/.steam/root/compatibilitytools.d";
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
   };
+
+ programs.gamemode.enable = true;
+
+  hardware.graphics.enable32Bit = true;
 
   home-manager.users.zozano = {
-    systemd.user.services.steam-bigpicture = {
-      Unit = {
-        Description = "Autostart Steam in Big Picture mode once XWayland is ready";
-        After = ["graphical-session.target"];
-        PartOf = ["graphical-session.target"];
-      };
-      Service = {
-        Type = "simple";
-        ExecStartPre = "${pkgs.writeShellScript "wait-for-xwayland" ''
-          until ${pkgs.xset}/bin/xset q >/dev/null 2>&1; do
-            sleep 0.5
-          done
-        ''}";
-        ExecStart = "${pkgs.steam}/bin/steam -silent %U";
-        Restart = "on-failure";
-        RestartSec = 2;
-      };
-      Install = {
-        WantedBy = ["graphical-session.target"];
-      };
-    };
+    # systemd.user.services.steam-bigpicture = {
+    #   Unit = {
+    #     Description = "Autostart Steam in Big Picture mode once XWayland is ready";
+    #     After = ["graphical-session.target"];
+    #     PartOf = ["graphical-session.target"];
+    #   };
+    #   Service = {
+    #     Type = "simple";
+    #     ExecStartPre = "${pkgs.writeShellScript "wait-for-xwayland" ''
+    #       until ${pkgs.xset}/bin/xset q >/dev/null 2>&1; do
+    #         sleep 0.5
+    #       done
+    #     ''}";
+    #     ExecStart = "${pkgs.steam}/bin/steam -silent %U";
+    #     Restart = "on-failure";
+    #     RestartSec = 2;
+    #   };
+    #   Install = {
+    #     WantedBy = ["graphical-session.target"];
+    #   };
+    # };
 
     home.packages = with pkgs; [
+      mangohud
+      gnomeExtensions.appindicator
       protonup-qt
+    ];
+
+    dconf.settings."org/gnome/shell".enabled-extensions = [
+      "appindicatorsupport@rgcjonas.gmail.com"
     ];
   };
 }
